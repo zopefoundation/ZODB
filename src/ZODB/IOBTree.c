aff12e69025f47bc29ef831725a35bc1d356699c
Jim Fulton <jim@zope.com>
1997-10-10 18:24:35 +0000

    Added extra ID string to make make release happy.

--- src/ZODB/IOBTree.c
+++ src/ZODB/IOBTree.c
@@ -1,4 +1,7 @@
-/* $Id: IOBTree.c,v 1.3 1997/09/24 19:21:06 brian Exp $ */
+/* $Id: IOBTree.c,v 1.4 1997/10/10 18:24:11 jim Exp $ */
+
+static char *needed_to_make_release_happy=
+"$Id: IOBTree.c,v 1.4 1997/10/10 18:24:11 jim Exp $";
 
 #define INTKEY int
 #include "BTree.c"
--- src/ZODB/OIBTree.c
+++ src/ZODB/OIBTree.c
@@ -1,4 +1,7 @@
-/* $Id: OIBTree.c,v 1.3 1997/09/24 19:21:07 brian Exp $ */
+/* $Id: OIBTree.c,v 1.4 1997/10/10 18:24:35 jim Exp $ */
+
+static char *needed_to_make_release_happy=
+"$Id: OIBTree.c,v 1.4 1997/10/10 18:24:35 jim Exp $";
 
 #define INTVAL int
 #include "BTree.c"
