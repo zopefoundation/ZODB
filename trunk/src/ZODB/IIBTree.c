65fc8988c23d1b46d8adae48ca1afedf738eab32
Jim Fulton <jim@zope.com>
1997-10-10 18:23:37 +0000

    Fixed bug in above.

--- trunk/src/ZODB/IIBTree.c
+++ trunk/src/ZODB/IIBTree.c
@@ -1,8 +1,8 @@
-/* $Id: IIBTree.c,v 1.3 1997/10/10 18:23:14 jim Exp $ */
+/* $Id: IIBTree.c,v 1.4 1997/10/10 18:23:37 jim Exp $ */
 
 #define INTKEY int
 #define INTVAL int
 
-static char *needed_to_make_release_happy="$Id";
+static char *needed_to_make_release_happy="$Id: IIBTree.c,v 1.4 1997/10/10 18:23:37 jim Exp $";
 
 #include "BTree.c"
