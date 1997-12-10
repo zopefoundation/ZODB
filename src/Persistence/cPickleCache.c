4032174c745355f8d67eb775b7a069c8593fc50e
Jim Fulton <jim@zope.com>
1997-12-10 22:20:43 +0000

    Added has_key method.

--- src/Persistence/cPickleCache.c
+++ src/Persistence/cPickleCache.c
@@ -1,62 +1,17 @@
 /*
 
-  $Id: cPickleCache.c,v 1.10 1997/07/18 14:30:18 jim Exp $
+  $Id: cPickleCache.c,v 1.11 1997/12/10 22:20:43 jim Exp $
 
   C implementation of a pickle jar cache.
-
 
      Copyright 
 
        Copyright 1996 Digital Creations, L.C., 910 Princess Anne
        Street, Suite 300, Fredericksburg, Virginia 22401 U.S.A. All
-       rights reserved.  Copyright in this software is owned by DCLC,
-       unless otherwise indicated. Permission to use, copy and
-       distribute this software is hereby granted, provided that the
-       above copyright notice appear in all copies and that both that
-       copyright notice and this permission notice appear. Note that
-       any product, process or technology described in this software
-       may be the subject of other Intellectual Property rights
-       reserved by Digital Creations, L.C. and are not licensed
-       hereunder.
-
-     Trademarks 
-
-       Digital Creations & DCLC, are trademarks of Digital Creations, L.C..
-       All other trademarks are owned by their respective companies. 
-
-     No Warranty 
-
-       The software is provided "as is" without warranty of any kind,
-       either express or implied, including, but not limited to, the
-       implied warranties of merchantability, fitness for a particular
-       purpose, or non-infringement. This software could include
-       technical inaccuracies or typographical errors. Changes are
-       periodically made to the software; these changes will be
-       incorporated in new editions of the software. DCLC may make
-       improvements and/or changes in this software at any time
-       without notice.
-
-     Limitation Of Liability 
-
-       In no event will DCLC be liable for direct, indirect, special,
-       incidental, economic, cover, or consequential damages arising
-       out of the use of or inability to use this software even if
-       advised of the possibility of such damages. Some states do not
-       allow the exclusion or limitation of implied warranties or
-       limitation of liability for incidental or consequential
-       damages, so the above limitation or exclusion may not apply to
-       you.
-
-    If you have questions regarding this software,
-    contact:
-   
-      Jim Fulton, jim@digicool.com
-      Digital Creations L.C.  
-   
-      (540) 371-6909
+       rights reserved.
 
 ***************************************************************************/
-static char *what_string = "$Id: cPickleCache.c,v 1.10 1997/07/18 14:30:18 jim Exp $";
+static char *what_string = "$Id: cPickleCache.c,v 1.11 1997/12/10 22:20:43 jim Exp $";
 
 #define ASSIGN(V,E) {PyObject *__e; __e=(E); Py_XDECREF(V); (V)=__e;}
 #define UNLESS(E) if(!(E))
@@ -468,6 +423,8 @@ cc_getattr(ccobject *self, char *name)
       if(strcmp(name,"cache_last_gc_time")==0)
 	return PyFloat_FromDouble(self->last_check);
     }
+  if(*name=='h' && strcmp(name, "has_key")==0)
+    return PyObject_GetAttrString(self->data, name);
 
   if(r=Py_FindMethod(cc_methods, (PyObject *)self, name))
     return r;
@@ -655,7 +612,7 @@ void
 initcPickleCache()
 {
   PyObject *m, *d;
-  char *rev="$Revision: 1.10 $";
+  char *rev="$Revision: 1.11 $";
 
   Cctype.ob_type=&PyType_Type;
 
@@ -683,6 +640,9 @@ initcPickleCache()
 
 /******************************************************************************
  $Log: cPickleCache.c,v $
+ Revision 1.11  1997/12/10 22:20:43  jim
+ Added has_key method.
+
  Revision 1.10  1997/07/18 14:30:18  jim
  Added reporting method for use during debugging.
 
