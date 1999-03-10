1b9f61cb20f09785f994c25fc6f00ebde353d3ae
Ken Manheimer <ken.manheimer@gmail.com>
1999-03-10 00:15:00 +0000

    Committing with version 1.0 of the license.

--- trunk/src/ZODB/winlock.c
+++ trunk/src/ZODB/winlock.c
@@ -1,88 +1,88 @@
 /*****************************************************************************
- 
-  Zope Public License (ZPL) Version 0.9.4
-  ---------------------------------------
+  
+  Zope Public License (ZPL) Version 1.0
+  -------------------------------------
   
   Copyright (c) Digital Creations.  All rights reserved.
   
-  Redistribution and use in source and binary forms, with or
-  without modification, are permitted provided that the following
-  conditions are met:
+  This license has been certified as Open Source(tm).
   
-  1. Redistributions in source code must retain the above
-     copyright notice, this list of conditions, and the following
-     disclaimer.
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
   
-  2. Redistributions in binary form must reproduce the above
-     copyright notice, this list of conditions, and the following
-     disclaimer in the documentation and/or other materials
-     provided with the distribution.
+  1. Redistributions in source code must retain the above copyright
+     notice, this list of conditions, and the following disclaimer.
   
-  3. Any use, including use of the Zope software to operate a
-     website, must either comply with the terms described below
-     under "Attribution" or alternatively secure a separate
-     license from Digital Creations.
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions, and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
   
-  4. All advertising materials, documentation, or technical papers
-     mentioning features derived from or use of this software must
-     display the following acknowledgement:
+  3. Digital Creations requests that attribution be given to Zope
+     in any manner possible. Zope includes a "Powered by Zope"
+     button that is installed by default. While it is not a license
+     violation to remove this button, it is requested that the
+     attribution remain. A significant investment has been put
+     into Zope, and this effort will continue if the Zope community
+     continues to grow. This is one way to assure that growth.
   
-       "This product includes software developed by Digital
-       Creations for use in the Z Object Publishing Environment
+  4. All advertising materials and documentation mentioning
+     features derived from or use of this software must display
+     the following acknowledgement:
+  
+       "This product includes software developed by Digital Creations
+       for use in the Z Object Publishing Environment
        (http://www.zope.org/)."
   
-  5. Names associated with Zope or Digital Creations must not be
-     used to endorse or promote products derived from this
-     software without prior written permission from Digital
-     Creations.
+     In the event that the product being advertised includes an
+     intact Zope distribution (with copyright and license included)
+     then this clause is waived.
   
-  6. Redistributions of any form whatsoever must retain the
-     following acknowledgment:
+  5. Names associated with Zope or Digital Creations must not be used to
+     endorse or promote products derived from this software without
+     prior written permission from Digital Creations.
   
-       "This product includes software developed by Digital
-       Creations for use in the Z Object Publishing Environment
+  6. Modified redistributions of any form whatsoever must retain
+     the following acknowledgment:
+  
+       "This product includes software developed by Digital Creations
+       for use in the Z Object Publishing Environment
        (http://www.zope.org/)."
   
-  7. Modifications are encouraged but must be packaged separately
-     as patches to official Zope releases.  Distributions that do
-     not clearly separate the patches from the original work must
-     be clearly labeled as unofficial distributions.
+     Intact (re-)distributions of any official Zope release do not
+     require an external acknowledgement.
+  
+  7. Modifications are encouraged but must be packaged separately as
+     patches to official Zope releases.  Distributions that do not
+     clearly separate the patches from the original work must be clearly
+     labeled as unofficial distributions.  Modifications which do not
+     carry the name Zope may be packaged in any form, as long as they
+     conform to all of the clauses above.
+  
   
   Disclaimer
   
-    THIS SOFTWARE IS PROVIDED BY DIGITAL CREATIONS ``AS IS'' AND
-    ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT
-    SHALL DIGITAL CREATIONS OR ITS CONTRIBUTORS BE LIABLE FOR ANY
-    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-    PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-    IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-    THE POSSIBILITY OF SUCH DAMAGE.
+    THIS SOFTWARE IS PROVIDED BY DIGITAL CREATIONS ``AS IS'' AND ANY
+    EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+    PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL DIGITAL CREATIONS OR ITS
+    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+    USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+    OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+    SUCH DAMAGE.
   
-  Attribution
   
-    Individuals or organizations using this software as a web site
-    must provide attribution by placing the accompanying "button"
-    and a link to the accompanying "credits page" on the website's
-    main entry point.  In cases where this placement of
-    attribution is not feasible, a separate arrangment must be
-    concluded with Digital Creations.  Those using the software
-    for purposes other than web sites must provide a corresponding
-    attribution in locations that include a copyright using a
-    manner best suited to the application environment.
-  
-  This software consists of contributions made by Digital
-  Creations and many individuals on behalf of Digital Creations.
-  Specific attributions are listed in the accompanying credits
-  file.
+  This software consists of contributions made by Digital Creations and
+  many individuals on behalf of Digital Creations.  Specific
+  attributions are listed in the accompanying credits file.
   
  ****************************************************************************/
-static char *what_string = "$Id: winlock.c,v 1.3 1999/02/05 00:45:26 jim Exp $";
+static char *what_string = "$Id: winlock.c,v 1.4 1999/03/10 00:15:00 klm Exp $";
 
 #include "Python.h"
 
@@ -136,7 +136,7 @@ static struct PyMethodDef methods[] = {
 DL_EXPORT(void)
 initwinlock() {
   PyObject *m, *d;
-  char *rev="$Revision: 1.3 $";
+  char *rev="$Revision: 1.4 $";
 
   if (! (Error=PyString_FromString("winlock.error"))) return;
 
