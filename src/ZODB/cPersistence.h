eb5c2b5429217b51cea0f3bb2410794abf90c0a2
Jim Fulton <jim@zope.com>
1999-09-16 14:02:08 +0000

    Combined macros onto one line to keep from messing up gdb.
    Fixed memory leak caused by incomplete PER_DEL.

--- src/ZODB/cPersistence.h
+++ src/ZODB/cPersistence.h
@@ -89,14 +89,7 @@
 #include "ExtensionClass.h"
 #include <time.h>
 
-#define cPersistent_HEAD   PyObject_HEAD \
-  PyObject *jar; \
-  PyObject *oid; \
-  char serial[8]; \
-  unsigned short atime; \
-  signed char state; \
-  unsigned char reserved; \
-
+#define cPersistent_HEAD PyObject_HEAD PyObject *jar, *oid; char serial[8]; unsigned short atime;  signed char state;  unsigned char reserved; 
 #define cPersistent_GHOST_STATE -1
 #define cPersistent_UPTODATE_STATE 0
 #define cPersistent_CHANGED_STATE 1
@@ -125,25 +118,15 @@ static cPersistenceCAPIstruct *cPersistenceCAPI;
 
 #define PERSISTENT_TYPE_FLAG EXTENSIONCLASS_USER_FLAG8
 
-#define PER_USE_OR_RETURN(O,R) {                        \
-  if ((O)->state==cPersistent_GHOST_STATE &&            \
-      cPersistenceCAPI->setstate((PyObject*)(O)) < 0)	\
-    return (R);						\
-  else if ((O)->state==cPersistent_UPTODATE_STATE)	\
-    (O)->state=cPersistent_STICKY_STATE;		\
-}
+#define PER_USE_OR_RETURN(O,R) {if((O)->state==cPersistent_GHOST_STATE && cPersistenceCAPI->setstate((PyObject*)(O)) < 0) return (R); else if ((O)->state==cPersistent_UPTODATE_STATE) (O)->state=cPersistent_STICKY_STATE;}
 
 #define PER_CHANGED(O) (cPersistenceCAPI->changed((cPersistentObject*)(O)))
 
-#define PER_ALLOW_DEACTIVATION(O)           \
-((O)->state==cPersistent_STICKY_STATE &&    \
- ((O)->state=cPersistent_UPTODATE_STATE)) 
+#define PER_ALLOW_DEACTIVATION(O) ((O)->state==cPersistent_STICKY_STATE && ((O)->state=cPersistent_UPTODATE_STATE)) 
 
-#define PER_PREVENT_DEACTIVATION(O)           \
-((O)->state==cPersistent_UPTODATE_STATE &&    \
- ((O)->state=cPersistent_STICKY_STATE)) 
+#define PER_PREVENT_DEACTIVATION(O)  ((O)->state==cPersistent_UPTODATE_STATE && ((O)->state=cPersistent_STICKY_STATE)) 
 
-#define PER_DEL(O) Py_XDECREF((O)->jar)
+#define PER_DEL(O) Py_XDECREF((O)->jar); Py_XDECREF((O)->oid);
 
 #endif
 
