1fe21e1f9e8195948be214d5884d7e334c869324
Christopher Petrilli <petrilli@amber.org>
1999-11-16 15:29:16 +0000

    Cleaned up license.

--- src/ZEO/ClientStorage.py
+++ src/ZEO/ClientStorage.py
@@ -1,53 +1,48 @@
-##############################################################################
-#
-# Copyright (c) 1996-1998, Digital Creations, Fredericksburg, VA, USA.
-# All rights reserved.
+######################################################################
+# Digital Creations Options License Version 0.9.0
+# -----------------------------------------------
 # 
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
+# Copyright (c) 1999, Digital Creations.  All rights reserved.
 # 
-#   o Redistributions of source code must retain the above copyright
-#     notice, this list of conditions, and the disclaimer that follows.
+# This license covers Zope software delivered as "options" by Digital
+# Creations.
 # 
-#   o Redistributions in binary form must reproduce the above copyright
-#     notice, this list of conditions, and the following disclaimer in
-#     the documentation and/or other materials provided with the
-#     distribution.
+# Use in source and binary forms, with or without modification, are
+# permitted provided that the following conditions are met:
 # 
-#   o Neither the name of Digital Creations nor the names of its
-#     contributors may be used to endorse or promote products derived
-#     from this software without specific prior written permission.
+# 1. Redistributions are not permitted in any form.
 # 
+# 2. This license permits one copy of software to be used by up to five
+#    developers in a single company. Use by more than five developers
+#    requires additional licenses.
 # 
-# THIS SOFTWARE IS PROVIDED BY DIGITAL CREATIONS AND CONTRIBUTORS *AS IS*
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL DIGITAL
-# CREATIONS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-# DAMAGE.
-#
+# 3. Software may be used to operate any type of website, including
+#    publicly accessible ones.
 # 
-# If you have questions regarding this software, contact:
-#
-#   Digital Creations, L.C.
-#   910 Princess Ann Street
-#   Fredericksburge, Virginia  22401
-#
-#   info@digicool.com
-#
-#   (540) 371-6909
-#
-##############################################################################
+# 4. Software is not fully documented, and the customer acknowledges
+#    that the product can best be utilized by reading the source code.
+# 
+# 5. Support for software is included for 90 days in email only. Further
+#    support can be purchased separately.
+# 
+# Disclaimer
+# 
+#   THIS SOFTWARE IS PROVIDED BY DIGITAL CREATIONS ``AS IS'' AND ANY
+#   EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+#   PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL DIGITAL CREATIONS OR ITS
+#   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+#   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+#   OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+#   SUCH DAMAGE.
+######################################################################
 """Network ZODB storage client
 """
-__version__='$Revision: 1.4 $'[11:-2]
+__version__='$Revision: 1.5 $'[11:-2]
 
 import struct, time, os, socket, cPickle, string, Sync, zrpc, ClientCache
 import tempfile
