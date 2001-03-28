458dfb60e4420a655937a71081f214098c497f86
Jim Fulton <jim@zope.com>
2001-03-28 13:52:42 +0000

    Undid previous change, which broke all extensions that defined persistent base classes (e.g. BTrees.)

--- trunk/src/persistent/cPersistence.h
+++ trunk/src/persistent/cPersistence.h
@@ -112,6 +112,10 @@ typedef struct {
   persetattr persetattro;
 } cPersistenceCAPIstruct;
 
+static cPersistenceCAPIstruct *cPersistenceCAPI;
+
+#define cPersistanceModuleName "cPersistence"
+
 #define PERSISTENT_TYPE_FLAG EXTENSIONCLASS_USER_FLAG8
 
 /* ExtensionClass class flags for persistent base classes should
