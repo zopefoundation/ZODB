0be6d6ea60c6d778d15fc24bba394eb2a97c8a18
Jeremy Hylton <jeremy@svn.zope.org>
2001-08-16 20:32:19 +0000

    Add rudiments of a ZEO test framework for Windows
    winserver.py: A small script that runs a ZEO StorageServer and a ZEOTestServer.
        The ZEOTestServer is used to kill the StorageServer during a unit test's
        teardown phases.  As soon as a client connects to that server, it exits
        the process.
    
    forker.py: Now forks and spawns, depending on os.
        XXX This needs to be cleaned up a lot, since the interfaces on Windows and
        Unix are nothing alike.
    
    testZEO.py:  Add WindowsGenericTests and WindowsZEOFileStorageTests.
        These test cases use the Windows-specific forker interface.  They also
        use getStorageInfo() to describe the storage, rather than getStorage()
        to create the storage.  This is necessary because the storage instance
        is created in a separate process.

--- src/ZEO/tests/forker.py
+++ src/ZEO/tests/forker.py
@@ -4,7 +4,10 @@ import asyncore
 import atexit
 import os
 import profile
+import random
+import socket
 import sys
+import threading
 import time
 import types
 import ThreadedAsync
@@ -12,80 +15,98 @@ import ZEO.ClientStorage, ZEO.StorageServer
 
 PROFILE = 0
 
-class ZEOServerExit(asyncore.file_dispatcher):
-    """Used to exit ZEO.StorageServer when run is done"""
+if os.name == "nt":
 
-    def writable(self):
-        return 0
+    def start_zeo_server(storage_name, args):
+        """Start a ZEO server in a separate process.
 
-    def readable(self):
-        return 1
+        Returns the ZEO port, the test server port, and the pid.
+        """
+        import ZEO.tests.winserver
+        port = random.randrange(20000, 30000)
+        script = ZEO.tests.winserver.__file__
+        if script.endswith('.pyc'):
+            script = script[:-1]
+        args = (sys.executable, script, str(port), storage_name) + args
+        pid = os.spawnv(os.P_NOWAIT, sys.executable, args)
+        return ('localhost', port), ('localhost', port + 1), pid
 
-    def handle_read(self):
-        buf = self.recv(4)
-        if buf:
-            assert buf == "done"
+else:
+
+    class ZEOServerExit(asyncore.file_dispatcher):
+        """Used to exit ZEO.StorageServer when run is done"""
+
+        def writable(self):
+            return 0
+
+        def readable(self):
+            return 1
+
+        def handle_read(self):
+            buf = self.recv(4)
+            if buf:
+                assert buf == "done"
+                asyncore.socket_map.clear()
+
+        def handle_close(self):
             asyncore.socket_map.clear()
-        
-    def handle_close(self):
-        asyncore.socket_map.clear()
 
-class ZEOClientExit:
-    """Used by client to cause server to exit"""
-    def __init__(self, pipe):
-        self.pipe = pipe
+    class ZEOClientExit:
+        """Used by client to cause server to exit"""
+        def __init__(self, pipe):
+            self.pipe = pipe
 
-    def close(self):
-        os.write(self.pipe, "done")
+        def close(self):
+            os.write(self.pipe, "done")
 
-def start_zeo_server(storage, addr):
-    rd, wr = os.pipe()
-    pid = os.fork()
-    if pid == 0:
-        if PROFILE:
-            p = profile.Profile()
-            p.runctx("run_server(storage, addr, rd, wr)", globals(),
-                     locals())
-            p.dump_stats("stats.s.%d" % os.getpid())
+    def start_zeo_server(storage, addr):
+        rd, wr = os.pipe()
+        pid = os.fork()
+        if pid == 0:
+            if PROFILE:
+                p = profile.Profile()
+                p.runctx("run_server(storage, addr, rd, wr)", globals(),
+                         locals())
+                p.dump_stats("stats.s.%d" % os.getpid())
+            else:
+                run_server(storage, addr, rd, wr)
+            os._exit(0)
         else:
-            run_server(storage, addr, rd, wr)
-        os._exit(0)
-    else:
-        os.close(rd)
-        return pid, ZEOClientExit(wr)
+            os.close(rd)
+            return pid, ZEOClientExit(wr)
 
-def run_server(storage, addr, rd, wr):
-    # in the child, run the storage server
-    os.close(wr)
-    ZEOServerExit(rd)
-    serv = ZEO.StorageServer.StorageServer(addr, {'1':storage})
-    asyncore.loop()
-    storage.close()
-    if isinstance(addr, types.StringType):
-        os.unlink(addr)
+    def run_server(storage, addr, rd, wr):
+        # in the child, run the storage server
+        os.close(wr)
+        ZEOServerExit(rd)
+        serv = ZEO.StorageServer.StorageServer(addr, {'1':storage})
+        asyncore.loop()
+        storage.close()
+        if isinstance(addr, types.StringType):
+            os.unlink(addr)
 
-def start_zeo(storage, cache=None, cleanup=None, domain="AF_INET",
-              storage_id="1", cache_size=20000000):
-    """Setup ZEO client-server for storage.
+    def start_zeo(storage, cache=None, cleanup=None, domain="AF_INET",
+                  storage_id="1", cache_size=20000000):
+        """Setup ZEO client-server for storage.
 
-    Returns a ClientStorage instance and a ZEOClientExit instance.
+        Returns a ClientStorage instance and a ZEOClientExit instance.
 
-    XXX Don't know if os.pipe() will work on Windows.
-    """
+        XXX Don't know if os.pipe() will work on Windows.
+        """
 
-    if domain == "AF_INET":
-        import random
-        addr = '', random.randrange(2000, 3000)
-    elif domain == "AF_UNIX":
-        import tempfile
-        addr = tempfile.mktemp()
-    else:
-        raise ValueError, "bad domain: %s" % domain
+        if domain == "AF_INET":
+            import random
+            addr = '', random.randrange(2000, 3000)
+        elif domain == "AF_UNIX":
+            import tempfile
+            addr = tempfile.mktemp()
+        else:
+            raise ValueError, "bad domain: %s" % domain
 
-    pid, exit = start_zeo_server(storage, addr)
-    s = ZEO.ClientStorage.ClientStorage(addr, storage_id,
-                                        debug=1, client=cache,
-                                        cache_size=cache_size,
-                                        min_disconnect_poll=0.5)
-    return s, exit, pid
+        pid, exit = start_zeo_server(storage, addr)
+        s = ZEO.ClientStorage.ClientStorage(addr, storage_id,
+                                            debug=1, client=cache,
+                                            cache_size=cache_size,
+                                            min_disconnect_poll=0.5)
+        return s, exit, pid
 
