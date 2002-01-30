40998e10c4e1f428ae4e564021834b9059451331
Jeremy Hylton <jeremy@svn.zope.org>
2002-01-30 20:55:38 +0000

    Support -d option to specify days

--- src/scripts/zeopack.py
+++ src/scripts/zeopack.py
@@ -5,27 +5,29 @@ Usage: zeopack.py [options]
 
 Options:
 
-    -p -- port to connect to
+    -p port -- port to connect to
     
-    -h -- host to connect to (default is current host)
+    -h host -- host to connect to (default is current host)
     
-    -U -- Unix-domain socket to connect to
+    -U path -- Unix-domain socket to connect to
     
-    -S -- storage name (default is '1')
+    -S name -- storage name (default is '1')
+
+    -d days -- pack objects more than days old
 
 You must specify either -p and -h or -U.
 """
 
 from ZEO.ClientStorage import ClientStorage
 
-def main(addr, storage):
+def main(addr, storage, days):
     cs = ClientStorage(addr, storage=storage, wait_for_server_on_startup=1)
     # _startup() is an artifact of the way ZEO 1.0 works.  The
     # ClientStorage doesn't get fully initialized until registerDB()
     # is called.  The only thing we care about, though, is that
     # registerDB() calls _startup().
     cs._startup()
-    cs.pack(wait=1)
+    cs.pack(wait=1, days=0)
 
 def usage(exit=1):
     print __doc__
@@ -41,16 +43,23 @@ if __name__ == "__main__":
     port = None
     unix = None
     storage = '1'
-    opts, args = getopt.getopt(sys.argv[1:], 'p:h:U:S:')
-    for o, a in opts:
-        if o == '-p':
-            port = int(a)
-        elif o == '-h':
-            host = a
-        elif o == '-U':
-            unix = a
-        elif o == '-S':
-            storage = a
+    days = 0
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'p:h:U:S:d:')
+        for o, a in opts:
+            if o == '-p':
+                port = int(a)
+            elif o == '-h':
+                host = a
+            elif o == '-U':
+                unix = a
+            elif o == '-S':
+                storage = a
+            elif o == '-d':
+                days = int(a)
+    except Exception, err:
+        print err
+        usage()
 
     if unix is not None:
         addr = unix
@@ -61,4 +70,4 @@ if __name__ == "__main__":
             usage()
         addr = host, port
     
-    main(addr, storage)
+    main(addr, storage, days)
