9677fe8ebe3bcb91adbb990f9ab128f4d5ff0ad4
Martijn Pieters <mj@zopatista.co>
2002-08-14 22:02:00 +0000

    Clean up indentation and trailing whitespace.

--- trunk/src/ThreadedAsync/__init__.py
+++ trunk/src/ThreadedAsync/__init__.py
@@ -2,17 +2,17 @@
 #
 # Copyright (c) 2001, 2002 Zope Corporation and Contributors.
 # All Rights Reserved.
-# 
+#
 # This software is subject to the provisions of the Zope Public License,
 # Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE
-# 
+#
 ##############################################################################
 """
 """
-__version__='$Revision: 1.3 $'[11:-2]
+__version__='$Revision: 1.4 $'[11:-2]
 
 from LoopCallback import register_loop_callback
