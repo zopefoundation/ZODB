f1550de8c7274c8c5578ffa4f0640358c4d56313
Guido van Rossum <guido@python.org>
2002-09-19 20:47:44 +0000

    Update comments.  Explain that zrpc is an internal detail of ZEO.

--- trunk/src/ZEO/zrpc/__init__.py
+++ trunk/src/ZEO/zrpc/__init__.py
@@ -12,9 +12,13 @@
 #
 ##############################################################################
 # zrpc is a package with the following modules
+# client -- manages connection creation to remote server
+# connection -- object dispatcher
+# log -- logging helper
 # error -- exceptions raised by zrpc
 # marshal -- internal, handles basic protocol issues
-# connection -- object dispatcher
-# client -- manages connection creation to remote server
 # server -- manages incoming connections from remote clients
+# smac -- sized message async connections
 # trigger -- medusa's trigger
+
+# zrpc is not an advertised subpackage of ZEO; its interfaces are internal
