c3dfded02db9f230e0ad7c63a1eb5bf242088e67
Jeremy Hylton <jeremy@svn.zope.org>
2002-10-01 16:04:52 +0000

    Appease the Society to Promote Whitespace.

--- trunk/src/ZODB/__init__.py
+++ trunk/src/ZODB/__init__.py
@@ -20,25 +20,25 @@ from zLOG import register_subsystem
 register_subsystem('ZODB')
 
 # This is lame. Don't look. :(
-sys.modules['cPersistence']=cPersistence
+sys.modules['cPersistence'] = cPersistence
 
-Persistent=cPersistence.Persistent
+Persistent = cPersistence.Persistent
 
 # Install Persistent and PersistentMapping in Persistence
 if not hasattr(Persistence, 'Persistent'):
-    Persistence.Persistent=Persistent
-    Persistent.__module__='Persistence'
-    Persistence.Overridable=cPersistence.Overridable
-    Persistence.Overridable.__module__='Persistence'
+    Persistence.Persistent = Persistent
+    Persistent.__module__ = 'Persistence'
+    Persistence.Overridable = cPersistence.Overridable
+    Persistence.Overridable.__module__ = 'Persistence'
     if not hasattr(Persistence, 'PersistentMapping'):
         import PersistentMapping
-        sys.modules['PersistentMapping']=PersistentMapping
-        sys.modules['BoboPOS']=sys.modules['ZODB']
-        sys.modules['BoboPOS.PersistentMapping']=PersistentMapping
-        PersistentMapping=PersistentMapping.PersistentMapping
+        sys.modules['PersistentMapping'] = PersistentMapping
+        sys.modules['BoboPOS'] = sys.modules['ZODB']
+        sys.modules['BoboPOS.PersistentMapping'] = PersistentMapping
+        PersistentMapping = PersistentMapping.PersistentMapping
         from PersistentMapping import PersistentMapping
-        Persistence.PersistentMapping=PersistentMapping
-        PersistentMapping.__module__='Persistence'
+        Persistence.PersistentMapping = PersistentMapping
+        PersistentMapping.__module__ = 'Persistence'
         del PersistentMapping
 
 del cPersistence
