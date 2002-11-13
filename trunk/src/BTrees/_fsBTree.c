84028d1d956bc35a06ba256b777ab7841fb3d789
Jeremy Hylton <jeremy@svn.zope.org>
2002-11-13 16:32:03 +0000

    Fix more includes before Python.h.

--- trunk/src/BTrees/_fsBTree.c
+++ trunk/src/BTrees/_fsBTree.c
@@ -7,15 +7,13 @@
    positions.
 */
 
-#include <string.h>
-
 typedef unsigned char char2[2];
 typedef unsigned char char6[6];
 
 
 /* Setup template macros */
 
-#define MASTER_ID "$Id: _fsBTree.c,v 1.4 2002/06/27 00:32:54 tim_one Exp $\n"
+#define MASTER_ID "$Id: _fsBTree.c,v 1.5 2002/11/13 16:32:03 jeremy Exp $\n"
 
 #define PERSISTENT
 
@@ -26,7 +24,7 @@ typedef unsigned char char6[6];
                 
 /*#include "intkeymacros.h"*/
 
-#define KEYMACROS_H "$Id: _fsBTree.c,v 1.4 2002/06/27 00:32:54 tim_one Exp $\n"
+#define KEYMACROS_H "$Id: _fsBTree.c,v 1.5 2002/11/13 16:32:03 jeremy Exp $\n"
 #define KEY_TYPE char2
 #undef KEY_TYPE_IS_PYOBJECT
 #define KEY_CHECK(K) (PyString_Check(K) && PyString_GET_SIZE(K)==2)
@@ -41,7 +39,7 @@ typedef unsigned char char6[6];
       (STATUS)=0; } 
 
 /*#include "intvaluemacros.h"*/
-#define VALUEMACROS_H "$Id: _fsBTree.c,v 1.4 2002/06/27 00:32:54 tim_one Exp $\n"
+#define VALUEMACROS_H "$Id: _fsBTree.c,v 1.5 2002/11/13 16:32:03 jeremy Exp $\n"
 #define VALUE_TYPE char6
 #undef VALUE_TYPE_IS_PYOBJECT
 #define TEST_VALUE(K, T) strncmp(K,T,6)
