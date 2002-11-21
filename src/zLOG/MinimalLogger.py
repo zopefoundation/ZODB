24942cb7761a7f77d2db2a57eab7238fdcad4262
Fred Drake <fdrake@acm.org>
2002-11-21 23:17:57 +0000

    - zLOG.severity():  Convenience function that converts a value to a   severity level.  This allows the common names to be used for   severities, not just integers.  This can make configuration data   less obscure.
    - Refactor initialize():
      get_environment_info() pulls information from the environment
      variables, and this gets passed to initialize_log(), which does
      nothing to determine the source of the configuration data.
    
    - New method initialize_with_config():  Load information not provided
      by the environment from a ZConfig section.  This allows the
      environment to override a config file, and avoids having the
      application deal with the specific information needed to configure
      logging.

--- src/zLOG/MinimalLogger.py
+++ src/zLOG/MinimalLogger.py
@@ -8,12 +8,13 @@
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE
+# FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-__version__='$Revision: 1.19 $'[11:-2]
+__version__='$Revision: 1.20 $'[11:-2]
 
 import os, sys, time
+import zLOG
 
 try:
     import textwrap
@@ -53,7 +54,39 @@ class stupid_log_write:
         self.initialize()
 
     def initialize(self):
+        path, severity = self.get_environment_info()
+        self.initialize_log(path, severity)
+
+    def initialize_with_config(self, config):
+        """Initialize logging with information from ZConfig."""
+        path, severity = self.get_environment_info()
+
+        if config is not None:
+            loginfo = config.getSection("log")
+            if loginfo is not None:
+                if path is None:
+                    path = loginfo.get("path")
+                if severity is None:
+                    severity = loginfo.get("level")
+
+        self.initialize_log(path, severity)
+
+    def initialize_log(self, path, severity):
         global _log_level
+
+        if path is None:
+            _set_log_dest(None)
+        elif path:
+            _set_log_dest(open(path, 'a'))
+        else:
+            _set_log_dest(sys.stderr)
+
+        if severity:
+            _log_level = zLOG.severity(severity)
+        else:
+            _log_level = 0 # INFO
+
+    def get_environment_info(self):
         eget = os.environ.get
 
         # EVENT_LOG_FILE is the preferred envvar, but we accept
@@ -61,21 +94,14 @@ class stupid_log_write:
         path = eget('EVENT_LOG_FILE')
         if path is None:
             path = eget('STUPID_LOG_FILE')
-        if path is None:
-            _set_log_dest(None)
-        else:
-            if path:
-                _set_log_dest(open(path, 'a'))
-            else:
-                _set_log_dest(sys.stderr)
 
         # EVENT_LOG_SEVERITY is the preferred envvar, but we accept
         # STUPID_LOG_SEVERITY also
-        severity = eget('EVENT_LOG_SEVERITY') or eget('STUPID_LOG_SEVERITY')
-        if severity:
-            _log_level = int(severity)
-        else:
-            _log_level = 0 # INFO
+        severity = eget('EVENT_LOG_SEVERITY')
+        if severity is None:
+            severity = eget('STUPID_LOG_SEVERITY')
+
+        return path, severity
 
     def log(self, subsystem, severity, summary, detail, error):
         if _log_dest is None or severity < _log_level:
@@ -112,3 +138,4 @@ class stupid_log_write:
 _log = stupid_log_write()
 log_write = _log.log
 initialize = _log.initialize
+initialize_with_config = _log.initialize_with_config
