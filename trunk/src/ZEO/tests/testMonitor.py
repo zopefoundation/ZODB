76ed5f2f3959a19869ea3c0b6d6f0d3bc13ae8d5
Jeremy Hylton <jeremy@svn.zope.org>
2003-01-15 21:23:17 +0000

    Extend the monitor test suite to actually parse the monitor output.

--- trunk/src/ZEO/tests/testMonitor.py
+++ trunk/src/ZEO/tests/testMonitor.py
@@ -13,7 +13,7 @@
 ##############################################################################
 """Test that the monitor produce sensible results.
 
-$Id: testMonitor.py,v 1.2 2003/01/13 21:43:24 tim_one Exp $
+$Id: testMonitor.py,v 1.3 2003/01/15 21:23:17 jeremy Exp $
 """
 
 import socket
@@ -21,6 +21,7 @@ import time
 import unittest
 
 from ZEO.tests.ConnectionTests import CommonSetupTearDown
+from ZEO.monitor import StorageStats
 
 class MonitorTests(CommonSetupTearDown):
 
@@ -39,6 +40,37 @@ class MonitorTests(CommonSetupTearDown):
         s.close()
         return "".join(L)
 
+    def parse(self, s):
+        # Return a list of StorageStats, one for each storage.
+        lines = s.split("\n")
+        self.assert_(lines[0].startswith("ZEO monitor server"))
+        # lines[1] is a date
+
+        # Break up rest of lines into sections starting with Storage:
+        # and ending with a blank line.
+        sections = []
+        cur = None
+        for line in lines[2:]:
+            if line.startswith("Storage:"):
+                cur = [line]
+            elif line:
+                cur.append(line)
+            else:
+                if cur is not None:
+                    sections.append(cur)
+                    cur = None
+        assert cur is None # bug in the test code if this fails
+
+        d = {}
+        for sect in sections:
+            hdr = sect[0]
+            key, value = hdr.split(":")
+            storage = int(value)
+            s = d[storage] = StorageStats()
+            s.parse("\n".join(sect[1:]))
+
+        return d
+        
     def getConfig(self, path, create, read_only):
         return """\
         <Storage>
@@ -53,7 +85,10 @@ class MonitorTests(CommonSetupTearDown):
         s = self.get_monitor_output()
         self.storage.close()
         self.assert_(s.find("monitor") != -1)
-        
+        d = self.parse(s)
+        stats = d[1]
+        self.assertEqual(stats.clients, 1)
+        self.assertEqual(stats.commits, 0)
 
 def test_suite():
     return unittest.makeSuite(MonitorTests)
