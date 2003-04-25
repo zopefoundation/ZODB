d51bdac5d66e2d826b301313a2b37dd1f69f8e49
Jeremy Hylton <jeremy@svn.zope.org>
2003-04-25 19:28:47 +0000

    Add missing copyright notices.

--- src/transaction/txn.py
+++ src/transaction/txn.py
@@ -1,5 +1,16 @@
-# XXX The fact that this module has the same name as the package makes
-# explicit imports impossible elsewhere.  Pick a new name?
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 
 __metaclass__ = type
 
