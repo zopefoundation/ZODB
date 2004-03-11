05aa60bbdf54e27fd2efa8d8326d5a3b6a6dcf01
Jeremy Hylton <jeremy@svn.zope.org>
2004-03-11 20:16:06 +0000

    Fix copyright date.

--- trunk/src/ZODB/tests/test_storage.py
+++ trunk/src/ZODB/tests/test_storage.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# Copyright (c) 2004 Zope Corporation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
