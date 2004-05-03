911e05ab3727d92fb1abbba8537b897f995e5fe0
Scott Pascoe <manager@planneditsolutions.com>
2004-05-03 18:45:52 +0000

    Spring Cleaning, Add ZPL and module comment

--- trunk/src/BTrees/_IOBTree.c
+++ trunk/src/BTrees/_IOBTree.c
@@ -1,5 +1,24 @@
+/*############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################*/
 
-#define MASTER_ID "$Id: _IOBTree.c,v 1.6 2003/11/28 16:44:44 jim Exp $\n"
+#define MASTER_ID "$Id: _IOBTree.c,v 1.7 2004/05/03 18:45:52 spascoe Exp $\n"
+
+/* IOBTree - int key, object value BTree
+
+   Implements a collection using int type keys
+   and object type values
+*/
 
 #define PERSISTENT
 
--- trunk/src/BTrees/_OIBTree.c
+++ trunk/src/BTrees/_OIBTree.c
@@ -1,5 +1,24 @@
+/*############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################*/
 
-#define MASTER_ID "$Id: _OIBTree.c,v 1.3 2003/11/28 16:44:44 jim Exp $\n"
+#define MASTER_ID "$Id: _OIBTree.c,v 1.4 2004/05/03 18:45:52 spascoe Exp $\n"
+
+/* OIBTree - object key, int value BTree
+
+   Implements a collection using object type keys
+   and int type values
+*/
 
 #define PERSISTENT
 
