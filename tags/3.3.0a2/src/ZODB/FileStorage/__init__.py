c394cc1a745962dec251b20a5b88796c33ad9dba
Jim Fulton <jim@zope.com>
2004-06-02 21:11:17 +0000

    Removing tag. I forgot to record the revision numer the tag was created from, dang it.

--- tags/3.3.0a2/src/ZODB/FileStorage/__init__.py
+++
@@ -1,4 +0,0 @@
-# this is a package
-
-from ZODB.FileStorage.FileStorage \
-     import FileStorage, RecordIterator, FileIterator, packed_version
--- tags/3.3.0a2/src/scripts/fsdump.py
+++
@@ -1,8 +0,0 @@
-#!python
-"""Print a text summary of the contents of a FileStorage."""
-
-from ZODB.FileStorage.fsdump import fsdump
-
-if __name__ == "__main__":
-    import sys
-    fsdump(sys.argv[1])
