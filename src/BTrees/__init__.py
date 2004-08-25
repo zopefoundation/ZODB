bc6751ac0532282059c770240746b4794ce72b9f
Tim Peters <tim.one@comcast.net>
2004-08-25 20:42:43 +0000

    Port rev 27272 from 3.3 branch.
    Empty BTrees/__init__.py; isn't doing anything except
    creating problems.

--- src/BTrees/__init__.py
+++ src/BTrees/__init__.py
@@ -1,16 +1,1 @@
-try:
-    import intSet
-except:
-    pass
-else:
-    del intSet
-
-# Register interfaces
-try:
-    import Interface
-except ImportError:
-    pass # Don't register interfaces if no scarecrow
-else:
-    import Interfaces
-    del Interfaces
-    del Interface
+# This is a Python package.
