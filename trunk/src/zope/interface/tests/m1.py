1fbb564c291ef41f894308ed3375e345d93cf6d1
Dmitry Vasiliev <dima@hlabs.spb.ru>
2004-10-12 10:48:16 +0000

    Prepare to update zope.interface, zope.testing from Zope 3 trunk

--- trunk/src/zope/interface/tests/m1.py
+++
@@ -1,9 +0,0 @@
-"""Test module that declares an interface
-"""
-
-from zope.interface import Interface, moduleProvides
-
-class I1(Interface): pass
-class I2(Interface): pass
-
-moduleProvides(I1, I2)
