c984770af4daebc943368fa477aed7a730f81f2f
Tim Peters <tim.one@comcast.net>
2005-01-11 19:22:15 +0000

    Changes for a 3.3.1a1 release.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ interface, rich transaction support, and undo.
 # Development Status :: 3 - Alpha
 
 classifiers = """\
-Development Status :: 5 - Production/Stable
+Development Status :: 3 - Alpha
 Intended Audience :: Developers
 License :: OSI Approved :: Zope Public License
 Programming Language :: Python
@@ -221,7 +221,7 @@ class MyDistribution(Distribution):
 doclines = __doc__.split("\n")
 
 setup(name="ZODB3",
-      version="3.3",
+      version="3.3.1a1",
       maintainer="Zope Corporation",
       maintainer_email="zodb-dev@zope.org",
       url = "http://www.zope.org/Wikis/ZODB",
