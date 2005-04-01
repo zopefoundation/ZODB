b8953def4447d951c1500b2d47ebc37579e8e075
Tim Peters <tim.one@comcast.net>
2005-04-01 21:12:37 +0000

    Fiddle stuff for a 3.4a1 release.

--- trunk/setup.py
+++ trunk/setup.py
@@ -26,7 +26,7 @@ interface, rich transaction support, and undo.
 # Development Status :: 3 - Alpha
 
 classifiers = """\
-Development Status :: 5 - Production/Stable
+Development Status :: 3 - Alpha
 Intended Audience :: Developers
 License :: OSI Approved :: Zope Public License
 Programming Language :: Python
@@ -247,7 +247,7 @@ class MyDistribution(Distribution):
 doclines = __doc__.split("\n")
 
 setup(name="ZODB3",
-      version="3.4a0",
+      version="3.4a1",
       maintainer="Zope Corporation",
       maintainer_email="zodb-dev@zope.org",
       url = "http://www.zope.org/Wikis/ZODB",
