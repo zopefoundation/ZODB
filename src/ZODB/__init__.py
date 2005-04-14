62b12b51cc9ca3951646d416e016e21d5843dd61
Tim Peters <tim.one@comcast.net>
2005-04-14 20:26:08 +0000

    3.3.1 final.
    Alas, after letting Cygwin update TeX components, it
    was impossible to regenerate the PSF or HTML docs --
    utterly incomprehsible msgs, like "I'm stymied", and
    then it quit.
    
    Backing off to some 2.0.0 version allowed getting the
    PDF generated, but it still can't create the HTML form.
    
    I don't have time to try to sort this out; the HTML
    will have to be done later (if ever).

--- src/ZODB/__init__.py
+++ src/ZODB/__init__.py
@@ -13,7 +13,7 @@
 ##############################################################################
 
 # The next line must use double quotes, so release.py recognizes it.
-__version__ = "3.3.1c1"
+__version__ = "3.3.1"
 
 import sys
 import __builtin__
