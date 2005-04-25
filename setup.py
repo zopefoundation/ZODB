00b57dc506b2bffaee0eec6c63fcfd14e862def4
Tim Peters <tim.one@comcast.net>
2005-04-25 18:17:37 +0000

    An internal 3.4a5 release, to incorporate savepoint fixes.

--- setup.py
+++ setup.py
@@ -247,7 +247,7 @@ class MyDistribution(Distribution):
 doclines = __doc__.split("\n")
 
 setup(name="ZODB3",
-      version="3.4.0a4",
+      version="3.4.0a5",
       maintainer="Zope Corporation",
       maintainer_email="zodb-dev@zope.org",
       url = "http://www.zope.org/Wikis/ZODB",
