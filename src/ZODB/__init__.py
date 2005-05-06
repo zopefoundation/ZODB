9b42a5e090139727a75f1d7a520a7c31a2e81560
Tim Peters <tim.one@comcast.net>
2005-05-06 01:25:58 +0000

    Make an internal 3.4a6 release, primarily for Zope 2.8. This is effectively a beta release, but I don't want to take the time to do a full-blown release right now.

--- src/ZODB/__init__.py
+++ src/ZODB/__init__.py
@@ -13,7 +13,7 @@
 ##############################################################################
 
 # The next line must use double quotes, so release.py recognizes it.
-__version__ = "3.4.0a5"
+__version__ = "3.4a6"
 
 import sys
 import __builtin__
