58930f0bdb40453cad355101f288bab279adfaff
Tim Peters <tim.one@comcast.net>
2005-05-09 14:48:19 +0000

    An internal 3.4a8 release, to test Zope2 with ZAapplication moved.

--- setup.py
+++ setup.py
@@ -247,7 +247,7 @@ class MyDistribution(Distribution):
 doclines = __doc__.split("\n")
 
 setup(name="ZODB3",
-      version="3.4a7",
+      version="3.4a8",
       maintainer="Zope Corporation",
       maintainer_email="zodb-dev@zope.org",
       url = "http://www.zope.org/Wikis/ZODB",
--- src/ZODB/__init__.py
+++ src/ZODB/__init__.py
@@ -13,7 +13,7 @@
 ##############################################################################
 
 # The next line must use double quotes, so release.py recognizes it.
-__version__ = "3.4a7"
+__version__ = "3.4a8"
 
 import sys
 import __builtin__
