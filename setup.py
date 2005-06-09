fe08afff0d17be2d4e321e534c681e9e9110e7e8
Tim Peters <tim.one@comcast.net>
2005-06-09 18:18:57 +0000

    With luck, ZODB 3.4 final.

--- setup.py
+++ setup.py
@@ -264,7 +264,7 @@ class MyDistribution(Distribution):
 doclines = __doc__.split("\n")
 
 setup(name="ZODB3",
-      version="3.4.0c2",
+      version="3.4.0",
       maintainer="Zope Corporation",
       maintainer_email="zodb-dev@zope.org",
       url = "http://www.zope.org/Wikis/ZODB",
