3fe1e074e443de736190261aadd5bbc275baba53
Chris McDonough <chrism@plope.com>
2005-06-12 21:48:17 +0000

    New pack implementation and more tests.

--- src/ZODB/Blobs/BlobStorage.py
+++ src/ZODB/Blobs/BlobStorage.py
@@ -108,57 +108,79 @@ class BlobStorage(ProxyBase):
             raise POSKeyError, "Not an existing blob."
         return filename
 
-    def _getNewestBlobSerial(self, oid):
-        blob_path = self._getBlobPath(oid)
-        serials = os.listdir(blob_path)
-        serials = [ os.path.join(blob_path, serial) for serial in serials ]
-        serials.sort(lambda x,y: cmp(os.stat(x).st_mtime, 
-                                     os.stat(y).st_mtime)
-                     )
+    def _packUndoing(self, packtime, referencesf):
 
-        # XXX the above sort is inadequate for files written within
-        # the same second at least under UNIX (st_mtime has a 1-second
-        # resolution).  We should really try to make it an invariant
-        # that the filenames be sortable instead.  This is the case
-        # right now due to ever-increasing tid values, but that's
-        # presumably an implementation detail, and also relies on the
-        # clock never going backwards.
+        # Walk over all existing revisions of all blob files and check
+        # if they are still needed by attempting to load the revision
+        # of that object from the database.  This is maybe the slowest
+        # possible way to do this, but it's safe.
 
-        return self._splitBlobFilename(serials[-1])[1]
+        # XXX we should be tolerant of "garbage" directories/files in
+        # the base_directory here.
+
+        for oid_repr in os.listdir(self.base_directory):
+            oid = utils.repr_to_oid(oid_repr)
+            oid_path = os.path.join(self.base_directory, oid_repr)
+            files = os.listdir(oid_path)
+            files.sort()
+
+            for filename in files:
+                filepath = os.path.join(oid_path, filename)
+                whatever, serial = self._splitBlobFilename(filepath)
+                try:
+                    fn = self._getCleanFilename(oid, serial)
+                    self.loadSerial(oid, serial)
+                except POSKeyError:
+                    os.unlink(filepath)
+
+            if not os.listdir(oid_path):
+                shutil.rmtree(oid_path)
+
+    def _packNonUndoing(self, packtime, referencesf):
+        for oid_repr in os.listdir(self.base_directory):
+            oid = utils.repr_to_oid(oid_repr)
+            oid_path = os.path.join(self.base_directory, oid_repr)
+            exists = True
+
+            try:
+                self.load(oid, None) # no version support
+            except (POSKeyError, KeyError):
+                exists = False
+
+            if exists:
+                files = os.listdir(oid_path)
+                files.sort()
+                latest = files[-1] # depends on ever-increasing tids
+                files.remove(latest)
+                for file in files:
+                    os.unlink(os.path.join(oid_path, file))
+            else:
+                shutil.rmtree(oid_path)
+                continue
+
+            if not os.listdir(oid_path):
+                shutil.rmtree(oid_path)
 
     def pack(self, packtime, referencesf):
         """Remove all unused oid/tid combinations."""
-        getProxiedObject(self).pack(packtime, referencesf)
+        unproxied = getProxiedObject(self)
 
+        # pack the underlying storage, which will allow us to determine
+        # which serials are current.
+        result = unproxied.pack(packtime, referencesf)
+
+        # perform a pack on blob data
         self._lock_acquire()
         try:
-            # Walk over all existing files and check if they are still needed
-            for filename in os.listdir(self.base_directory):
-                oid = utils.repr_to_oid(filename)
-                serial = self._getNewestBlobSerial(oid)
-                file_path = os.path.join(self.base_directory, filename)
-        
-                try:
-                    self.loadSerial(oid, serial)   # XXX Is that expensive?
-                except POSKeyError:
-                    # The object doesn't exist anymore at all. We can remove
-                    # everything belonging to that oid
-                    shutil.rmtree(file_path)
-                else:
-                    # The object still exists. We can remove everything but the
-                    # last recent object before pack time.
-                    serials = os.listdir(file_path)
-                    recent_candidate = \
-                            os.path.split(self._getCleanFilename(oid, serial))[1]
-                    serials.remove(recent_candidate)
-                    for serial_candidate in serials:
-                        cfname = os.path.join(file_path, serial_candidate)
-                        mtime = os.stat(cfname).st_mtime
-                        if mtime < packtime:
-                            os.unlink(cfname)
+            if unproxied.supportsUndo():
+                self._packUndoing(packtime, referencesf)
+            else:
+                self._packNonUndoing(packtime, referencesf)
         finally:
             self._lock_release()
-         
+
+        return result
+    
     def getSize(self):
         """Return the size of the database in bytes."""
         orig_size = getProxiedObject(self).getSize()
