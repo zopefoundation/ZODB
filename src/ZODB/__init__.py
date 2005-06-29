a1e6c1c5c994c11b6991f00ac4fec9fcf511c12a
Tim Peters <tim.one@comcast.net>
2005-06-29 20:16:36 +0000

    An internal 3.4.1a3 release.

--- src/ZODB/__init__.py
+++ src/ZODB/__init__.py
@@ -13,7 +13,7 @@
 ##############################################################################
 
 # The next line must use double quotes, so release.py recognizes it.
-__version__ = "3.4.1a1"
+__version__ = "3.4.1a2"
 
 import sys
 import __builtin__
