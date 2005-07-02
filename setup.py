ee4b2b7b920716d13c8db2c11dac9d50bb5420d2
Tim Peters <tim.one@comcast.net>
2005-07-02 23:42:14 +0000

    An internal 3.4.1a3 release.

--- setup.py
+++ setup.py
@@ -264,7 +264,7 @@ class MyDistribution(Distribution):
 doclines = __doc__.split("\n")
 
 setup(name="ZODB3",
-      version="3.4.1a2",
+      version="3.4.1a3",
       maintainer="Zope Corporation",
       maintainer_email="zodb-dev@zope.org",
       url = "http://www.zope.org/Wikis/ZODB",
