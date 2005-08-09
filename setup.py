522c27f92c7590c4f11b1934345bb43373ffe789
Tim Peters <tim.one@comcast.net>
2005-08-09 17:52:05 +0000

    Take a stab at 3.4.1 final.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ interface, rich transaction support, and undo.
 # Development Status :: 3 - Alpha
 
 classifiers = """\
-Development Status :: 4 - Beta
+Development Status :: 5 - Production/Stable
 Intended Audience :: Developers
 License :: OSI Approved :: Zope Public License
 Programming Language :: Python
@@ -264,7 +264,7 @@ class MyDistribution(Distribution):
 doclines = __doc__.split("\n")
 
 setup(name="ZODB3",
-      version="3.4.1b5",
+      version="3.4.1",
       maintainer="Zope Corporation",
       maintainer_email="zodb-dev@zope.org",
       url = "http://www.zope.org/Wikis/ZODB",
