396de4cd0f10c2867cae3deab96065a419107494
Tim Peters <tim.one@comcast.net>
2005-08-11 20:49:40 +0000

    An internal 3.5a7 release.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ import sys
 import zpkgsetup.setup
 
 # Note that release.py must be able to recognize the VERSION line.
-VERSION = "3.5a6"
+VERSION = "3.5.0a7"
 
 here = os.path.dirname(os.path.abspath(__file__))
 
