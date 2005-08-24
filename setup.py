cdcfa60e62b732072fb5c28925f56fb2cbc8d448
Fred Drake <fdrake@acm.org>
2005-08-24 16:39:42 +0000

    - switch to the zpkg trunk, and drop the code to walk the sources looking   for packaging metadata; zpkg can take care of that for us - restore the copyright year, which made sense - restore the VERSION constant and the comment I should have read

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #############################################################################
 #
-# Copyright (c) 2002 Zope Corporation and Contributors.
+# Copyright (c) 2005 Zope Corporation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -18,27 +18,18 @@ import zpkgsetup.package
 import zpkgsetup.publication
 import zpkgsetup.setup
 
+# Note that release.py must be able to recognize the VERSION line.
+VERSION = "3.5.0a42"
+
 
 here = os.path.dirname(os.path.abspath(__file__))
 
 context = zpkgsetup.setup.SetupContext(
-    "ZODB", "3.5.0a42", __file__)
+    "ZODB", VERSION, __file__)
 
 context.load_metadata(
     os.path.join(here,
                  zpkgsetup.publication.PUBLICATION_CONF))
 
-for root, dirs, files in os.walk("src"):
-    for d in dirs[:]:
-        # drop sub-directories that are not Python packages:
-        initfn = os.path.join(root, d, "__init__.py")
-        if not os.path.isfile(initfn):
-            dirs.remove(d)
-    if zpkgsetup.package.PACKAGE_CONF in files:
-        # scan this directory as a package:
-        pkgname = root[4:].replace(os.path.sep, ".")
-        local_full_path = os.path.join(here, root)
-        relative_path = root.replace(os.path.sep, "/")
-        context.scan(pkgname, local_full_path, relative_path)
-
+context.walk_packages("src")
 context.setup()
