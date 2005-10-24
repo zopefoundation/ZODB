d0207efad783ecd3f296353782d173f460c4f2af
Tim Peters <tim.one@comcast.net>
2005-10-24 16:14:22 +0000

    Add optional new databases= argument to the open() method of ZODBDatabase factories.  This gives apps (like Zopes 2.9 and 3.2) a relatively clean way to set up their multidatabases.  An earlier checkin adding an optional new database_name key to <zodb> sections.

--- src/ZODB/config.py
+++ src/ZODB/config.py
@@ -92,7 +92,7 @@ class BaseConfig:
 
 class ZODBDatabase(BaseConfig):
 
-    def open(self):
+    def open(self, databases=None):
         section = self.config
         storage = section.storage.open()
         try:
@@ -101,7 +101,8 @@ class ZODBDatabase(BaseConfig):
                            cache_size=section.cache_size,
                            version_pool_size=section.version_pool_size,
                            version_cache_size=section.version_cache_size,
-                           database_name=section.database_name)
+                           database_name=section.database_name,
+                           databases=databases)
         except:
             storage.close()
             raise
