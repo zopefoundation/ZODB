0ba197787d88b3a128b4b148e90a86dd923d2350
Tim Peters <tim.one@comcast.net>
2006-01-02 01:56:33 +0000

    An internal 3.6.0b6 release.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ import zpkgsetup.publication
 import zpkgsetup.setup
 
 # Note that release.py must be able to recognize the VERSION line.
-VERSION = "3.6.0b5"
+VERSION = "3.6.0b6"
 
 context = zpkgsetup.setup.SetupContext(
     "ZODB", VERSION, __file__)
--- src/ZODB/__init__.py
+++ src/ZODB/__init__.py
@@ -13,7 +13,7 @@
 ##############################################################################
 
 # The next line must use double quotes, so release.py recognizes it.
-__version__ = "3.6.0b5"
+__version__ = "3.6.0b6"
 
 import sys
 
