699068de0b9de94921a3c78e793da6d850c5c153
Andreas Jung <yet@gmx.de>
2006-05-26 04:11:56 +0000

    ZODB 3.4.3 (internal release used for Zope 2.8.7)

--- src/ZODB/__init__.py
+++ src/ZODB/__init__.py
@@ -13,7 +13,7 @@
 ##############################################################################
 
 # The next line must use double quotes, so release.py recognizes it.
-__version__ = "3.4.2"
+__version__ = "3.4.3"
 
 import sys
 import __builtin__
