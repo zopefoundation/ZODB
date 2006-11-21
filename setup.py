b3bcb361ce4dca0707c918d8c44161860fba3b1c
Jim Fulton <jim@zope.com>
2006-11-21 20:59:46 +0000

    Removed stupid partial change. Rassafrasm.

--- setup.py
+++ setup.py
@@ -20,8 +20,6 @@ to application logic.  ZODB includes features such as a plugable storage
 interface, rich transaction support, and undo.
 """
 
-DEPENDENCIES = 
-
 # The (non-obvious!) choices for the Trove Development Status line:
 # Development Status :: 5 - Production/Stable
 # Development Status :: 4 - Beta
