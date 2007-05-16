7186ec03d1c80e5f2988c73654f50f98119e22d0
Jim Fulton <jim@zope.com>
2007-05-16 15:11:49 +0000

    Updated winlock to use exception classes and created basic tests.

---
+++ src/ZODB/tests/testwinlock.py
@@ -0,0 +1,22 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+import os, sys, unittest
+from zope.testing import doctest
+
+def test_suite():
+    if sys.platform == 'win32':
+        return doctest.DocFileSuite(os.path.join('..', 'winlock.txt'))
+    else:
+        return unittest.TestSuite()
+
