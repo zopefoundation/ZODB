ac92c6efe553cd48cc5f2a01adcd4947b982ae83
Christian Theune <ct@gocept.com>
2007-09-21 06:37:25 +0000

    Bumping version b3 was released already.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ to application logic.  ZODB includes features such as a plugable storage
 interface, rich transaction support, and undo.
 """
 
-VERSION = "3.8.0b3"
+VERSION = "3.8.0b4"
 
 # The (non-obvious!) choices for the Trove Development Status line:
 # Development Status :: 5 - Production/Stable
