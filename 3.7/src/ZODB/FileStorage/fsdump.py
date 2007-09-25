8cee87cb11cdfb7976a641145c81e235c17dd337
Jim Fulton <jim@zope.com>
2007-09-25 12:10:38 +0000

    tagged

---
+++ 3.7/src/ZODB/FileStorage/fsdump.py
@@ -0,0 +1,136 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+import struct
+
+from ZODB.FileStorage import FileIterator
+from ZODB.FileStorage.format \
+     import TRANS_HDR, TRANS_HDR_LEN, DATA_HDR, DATA_HDR_LEN
+from ZODB.TimeStamp import TimeStamp
+from ZODB.utils import u64, get_pickle_metadata
+from ZODB.tests.StorageTestBase import zodb_unpickle
+
+def fsdump(path, file=None, with_offset=1):
+    iter = FileIterator(path)
+    for i, trans in enumerate(iter):
+        if with_offset:
+            print >> file, "Trans #%05d tid=%016x time=%s offset=%d" % \
+                  (i, u64(trans.tid), TimeStamp(trans.tid), trans._pos)
+        else:
+            print >> file, "Trans #%05d tid=%016x time=%s" % \
+                  (i, u64(trans.tid), TimeStamp(trans.tid))
+        print >> file, "    status=%r user=%r description=%r" % \
+              (trans.status, trans.user, trans.description)
+
+        for j, rec in enumerate(trans):
+            if rec.data is None:
+                fullclass = "undo or abort of object creation"
+                size = ""
+            else:
+                modname, classname = get_pickle_metadata(rec.data)
+                size = " size=%d" % len(rec.data)
+                fullclass = "%s.%s" % (modname, classname)
+
+            if rec.version:
+                version = " version=%r" % rec.version
+            else:
+                version = ""
+
+            if rec.data_txn:
+                # It would be nice to print the transaction number
+                # (i) but it would be expensive to keep track of.
+                bp = " bp=%016x" % u64(rec.data_txn)
+            else:
+                bp = ""
+
+            print >> file, "  data #%05d oid=%016x%s%s class=%s%s" % \
+                  (j, u64(rec.oid), version, size, fullclass, bp)
+    iter.close()
+
+def fmt(p64):
+    # Return a nicely formatted string for a packaged 64-bit value
+    return "%016x" % u64(p64)
+
+class Dumper:
+    """A very verbose dumper for debuggin FileStorage problems."""
+
+    # TODO:  Should revise this class to use FileStorageFormatter.
+
+    def __init__(self, path, dest=None):
+        self.file = open(path, "rb")
+        self.dest = dest
+
+    def dump(self):
+        fid = self.file.read(4)
+        print >> self.dest, "*" * 60
+        print >> self.dest, "file identifier: %r" % fid
+        while self.dump_txn():
+            pass
+
+    def dump_txn(self):
+        pos = self.file.tell()
+        h = self.file.read(TRANS_HDR_LEN)
+        if not h:
+            return False
+        tid, tlen, status, ul, dl, el = struct.unpack(TRANS_HDR, h)
+        end = pos + tlen
+        print >> self.dest, "=" * 60
+        print >> self.dest, "offset: %d" % pos
+        print >> self.dest, "end pos: %d" % end
+        print >> self.dest, "transaction id: %s" % fmt(tid)
+        print >> self.dest, "trec len: %d" % tlen
+        print >> self.dest, "status: %r" % status
+        user = descr = extra = ""
+        if ul:
+            user = self.file.read(ul)
+        if dl:
+            descr = self.file.read(dl)
+        if el:
+            extra = self.file.read(el)
+        print >> self.dest, "user: %r" % user
+        print >> self.dest, "description: %r" % descr
+        print >> self.dest, "len(extra): %d" % el
+        while self.file.tell() < end:
+            self.dump_data(pos)
+        stlen = self.file.read(8)
+        print >> self.dest, "redundant trec len: %d" % u64(stlen)
+        return 1
+
+    def dump_data(self, tloc):
+        pos = self.file.tell()
+        h = self.file.read(DATA_HDR_LEN)
+        assert len(h) == DATA_HDR_LEN
+        oid, revid, prev, tloc, vlen, dlen = struct.unpack(DATA_HDR, h)
+        print >> self.dest, "-" * 60
+        print >> self.dest, "offset: %d" % pos
+        print >> self.dest, "oid: %s" % fmt(oid)
+        print >> self.dest, "revid: %s" % fmt(revid)
+        print >> self.dest, "previous record offset: %d" % prev
+        print >> self.dest, "transaction offset: %d" % tloc
+        if vlen:
+            pnv = self.file.read(8)
+            sprevdata = self.file.read(8)
+            version = self.file.read(vlen)
+            print >> self.dest, "version: %r" % version
+            print >> self.dest, "non-version data offset: %d" % u64(pnv)
+            print >> self.dest, \
+                  "previous version data offset: %d" % u64(sprevdata)
+        print >> self.dest, "len(data): %d" % dlen
+        self.file.read(dlen)
+        if not dlen:
+            sbp = self.file.read(8)
+            print >> self.dest, "backpointer: %d" % u64(sbp)
+
+def main():
+    import sys
+    fsdump(sys.argv[1])
---
+++ 3.7/src/ZODB/tests/testfsoids.py
@@ -0,0 +1,177 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+r"""
+fsoids test, of the workhorse fsoids.Trace class
+================================================
+
+Let's get a temp file path to work with first.
+
+>>> import tempfile
+>>> path = tempfile.mktemp('.fs', 'Data')
+>>> print 'path:', path #doctest: +ELLIPSIS
+path: ...Data...fs
+
+More imports.
+
+>>> import ZODB
+>>> from ZODB.FileStorage import FileStorage
+>>> import transaction as txn
+>>> from BTrees.OOBTree import OOBTree
+>>> from ZODB.FileStorage.fsoids import Tracer  # we're testing this
+
+Create an empty FileStorage.
+
+>>> st = FileStorage(path)
+
+There's not a lot interesting in an empty DB!
+
+>>> t = Tracer(path)
+>>> t.register_oids(0x123456)
+>>> t.register_oids(1)
+>>> t.register_oids(0)
+>>> t.run()
+>>> t.report()
+oid 0x00 <unknown> 0 revisions
+    this oid was not defined (no data record for it found)
+oid 0x01 <unknown> 0 revisions
+    this oid was not defined (no data record for it found)
+oid 0x123456 <unknown> 0 revisions
+    this oid was not defined (no data record for it found)
+
+That didn't tell us much, but does show that the specified oids are sorted
+into increasing order.
+
+Create a root object and try again:
+
+>>> db = ZODB.DB(st) # yes, that creates a root object!
+>>> t = Tracer(path)
+>>> t.register_oids(0, 1)
+>>> t.run(); t.report() #doctest: +ELLIPSIS
+oid 0x00 persistent.mapping.PersistentMapping 1 revision
+    tid 0x... offset=4 ...
+        tid user=''
+        tid description='initial database creation'
+        new revision persistent.mapping.PersistentMapping at 52
+oid 0x01 <unknown> 0 revisions
+    this oid was not defined (no data record for it found)
+
+So we see oid 0 has been used in our one transaction, and that it was created
+there, and is a PersistentMapping.  4 is the file offset to the start of the
+transaction record, and 52 is the file offset to the start of the data record
+for oid 0 within this transaction.  Because tids are timestamps too, the
+"..." parts vary across runs.  The initial line for a tid actually looks like
+this:
+
+    tid 0x035748597843b877 offset=4 2004-08-20 20:41:28.187000
+
+Let's add a BTree and try again:
+
+>>> root = db.open().root()
+>>> root['tree'] = OOBTree()
+>>> txn.get().note('added an OOBTree')
+>>> txn.get().commit()
+>>> t = Tracer(path)
+>>> t.register_oids(0, 1)
+>>> t.run(); t.report() #doctest: +ELLIPSIS
+oid 0x00 persistent.mapping.PersistentMapping 2 revisions
+    tid 0x... offset=4 ...
+        tid user=''
+        tid description='initial database creation'
+        new revision persistent.mapping.PersistentMapping at 52
+    tid 0x... offset=168 ...
+        tid user=''
+        tid description='added an OOBTree'
+        new revision persistent.mapping.PersistentMapping at 207
+        references 0x01 BTrees._OOBTree.OOBTree at 207
+oid 0x01 BTrees._OOBTree.OOBTree 1 revision
+    tid 0x... offset=168 ...
+        tid user=''
+        tid description='added an OOBTree'
+        new revision BTrees._OOBTree.OOBTree at 363
+        referenced by 0x00 persistent.mapping.PersistentMapping at 207
+
+So there are two revisions of oid 0 now, and the second references oid 1.
+
+One more, storing a reference in the BTree back to the root object:
+
+>>> tree = root['tree']
+>>> tree['root'] = root
+>>> txn.get().note('circling back to the root')
+>>> txn.get().commit()
+>>> t = Tracer(path)
+>>> t.register_oids(0, 1, 2)
+>>> t.run(); t.report() #doctest: +ELLIPSIS
+oid 0x00 persistent.mapping.PersistentMapping 2 revisions
+    tid 0x... offset=4 ...
+        tid user=''
+        tid description='initial database creation'
+        new revision persistent.mapping.PersistentMapping at 52
+    tid 0x... offset=168 ...
+        tid user=''
+        tid description='added an OOBTree'
+        new revision persistent.mapping.PersistentMapping at 207
+        references 0x01 BTrees._OOBTree.OOBTree at 207
+    tid 0x... offset=443 ...
+        tid user=''
+        tid description='circling back to the root'
+        referenced by 0x01 BTrees._OOBTree.OOBTree at 491
+oid 0x01 BTrees._OOBTree.OOBTree 2 revisions
+    tid 0x... offset=168 ...
+        tid user=''
+        tid description='added an OOBTree'
+        new revision BTrees._OOBTree.OOBTree at 363
+        referenced by 0x00 persistent.mapping.PersistentMapping at 207
+    tid 0x... offset=443 ...
+        tid user=''
+        tid description='circling back to the root'
+        new revision BTrees._OOBTree.OOBTree at 491
+        references 0x00 persistent.mapping.PersistentMapping at 491
+oid 0x02 <unknown> 0 revisions
+    this oid was not defined (no data record for it found)
+
+Note that we didn't create any new object there (oid 2 is still unused), we
+just made oid 1 refer to oid 0.  Therefore there's a new "new revision" line
+in the output for oid 1.  Note that there's also new output for oid 0, even
+though the root object didn't change:  we got new output for oid 0 because
+it's a traced oid and the new transaction made a new reference *to* it.
+
+Since the Trace constructor takes only one argument, the only sane thing
+you can do to make it fail is to give it a path to a file that doesn't
+exist:
+
+>>> Tracer('/eiruowieuu/lsijflfjlsijflsdf/eurowiurowioeuri/908479287.fs')
+Traceback (most recent call last):
+  ...
+ValueError: must specify an existing FileStorage
+
+You get the same kind of exception if you pass it a path to an existing
+directory (the path must be to a file, not a directory):
+
+>>> import os
+>>> Tracer(os.path.dirname(__file__))
+Traceback (most recent call last):
+  ...
+ValueError: must specify an existing FileStorage
+
+
+Clean up.
+>>> st.close()
+>>> st.cleanup() # remove .fs, .index, etc
+"""
+
+from zope.testing import doctest
+
+def test_suite():
+    return doctest.DocTestSuite()
