904a38390b1d4db1a1838fc93c5494061dd853e9
Christian Theune <ct@gocept.com>
2007-11-10 16:06:06 +0000

    Extended output of fstail and provided a simple test. Fixes bug #158992.

--- src/ZODB/scripts/tests.py
+++ src/ZODB/scripts/tests.py
@@ -11,15 +11,21 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""XXX short summary goes here.
+"""Test harness for scripts.
 
 $Id$
 """
 import unittest
-from zope.testing import doctest
+import re
+from zope.testing import doctest, renormalizing
+
+checker = renormalizing.RENormalizing([
+    (re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]+'),
+     '2007-11-10 15:18:48.543001'),
+    (re.compile('hash=[0-9a-f]{40}'),
+     'hash=b16422d09fabdb45d4e4325e4b42d7d6f021d3c3')])
 
 def test_suite():
     return unittest.TestSuite((
-        doctest.DocFileSuite('referrers.txt'),
+        doctest.DocFileSuite('referrers.txt', 'fstail.txt', checker=checker),
         ))
-
