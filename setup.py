eac02e5ca98fdbad0b632e2e8e1bd91b0e31731a
Tres Seaver <tseaver@palladion.com>
2008-10-10 02:23:17 +0000

    Prep internal 3.4.5 release.

--- setup.py
+++ setup.py
@@ -264,7 +264,7 @@ class MyDistribution(Distribution):
 doclines = __doc__.split("\n")
 
 setup(name="ZODB3",
-      version="3.4.4",
+      version="3.4.5",
       maintainer="Zope Corporation",
       maintainer_email="zodb-dev@zope.org",
       url = "http://www.zope.org/Wikis/ZODB",
