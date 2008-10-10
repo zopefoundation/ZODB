0ca005fde1ddeb94b45abc50b3b0d49d9ff03ba3
Tres Seaver <tseaver@palladion.com>
2008-10-10 02:46:33 +0000

    Prep 3.6.4 release, to make LP 142667 fix available for Zope 2.9 branch.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ import zpkgsetup.publication
 import zpkgsetup.setup
 
 # Note that release.py must be able to recognize the VERSION line.
-VERSION = "3.6.3"
+VERSION = "3.6.4"
 
 context = zpkgsetup.setup.SetupContext(
     "ZODB", VERSION, __file__)
