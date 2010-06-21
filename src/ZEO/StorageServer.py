e3e65a080b010acb3029d5bdb3ca02600dc989ce
Christian Theune <ct@gocept.com>
2010-06-21 15:33:46 +0000

    Comply with repository policy.

--- src/ZEO/StorageServer.py
+++ src/ZEO/StorageServer.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2001, 2002, 2003 Zope Corporation and Contributors.
+# Copyright (c) 2001, 2002, 2003 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
