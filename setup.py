0e4aa0a5e8511a2b2ee3696d5fbdc4a14badd053
Jim Fulton <jim@zope.com>
2010-09-21 20:08:38 +0000

    Update version.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ to application logic.  ZODB includes features such as a plugable storage
 interface, rich transaction support, and undo.
 """
 
-VERSION = "3.9.0c2"
+VERSION = "3.9.6"
 
 from ez_setup import use_setuptools
 use_setuptools()
