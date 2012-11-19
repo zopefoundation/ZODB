b300f22745229fc1e3e7637f0e01feec0c2df173
Jim Fulton <jim@zope.com>
2012-11-19 19:37:05 +0000

    Include classifiers for python compat

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ classifiers = """\
 Intended Audience :: Developers
 License :: OSI Approved :: Zope Public License
 Programming Language :: Python
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
 Topic :: Database
 Topic :: Software Development :: Libraries :: Python Modules
 Operating System :: Microsoft :: Windows
