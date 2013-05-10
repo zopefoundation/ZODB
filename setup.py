3539498f148b2bcbe5b85a76108e3a9cab7241fb
Tres Seaver <tseaver@palladion.com>
2013-05-10 12:26:56 -0400

    No PyPy support yet. :(

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.2
 Programming Language :: Python :: 3.3
+Programming Language :: Python :: Implementation :: CPython
 Topic :: Database
 Topic :: Software Development :: Libraries :: Python Modules
 Operating System :: Microsoft :: Windows
