9cb624b69c5740644200e1a201713ac9f00666ab
Tres Seaver <tseaver@palladion.com>
2013-05-11 00:28:25 -0400

    Revert replacement of 'tempfile.mktemp'.
    Windows doesn't allow proper function with NamedTemporaryFile held open.

--- src/ZODB/tests/testConfig.py
+++ src/ZODB/tests/testConfig.py
@@ -61,28 +61,28 @@ class ZODBConfigTest(ConfigTestBase):
             """)
 
     def test_file_config1(self):
-        with tempfile.NamedTemporaryFile(prefix='Cfg') as f:
-            self._test(
-                """
-                <zodb>
-                <filestorage>
-                    path %s
-                </filestorage>
-                </zodb>
-                """ % f.name)
+        path = tempfile.mktemp()
+        self._test(
+            """
+            <zodb>
+              <filestorage>
+                path %s
+              </filestorage>
+            </zodb>
+            """ % path)
 
     def test_file_config2(self):
-        with tempfile.NamedTemporaryFile(prefix='Cfg') as f:
-            cfg = """
-            <zodb>
-            <filestorage>
-                path %s
-                create false
-                read-only true
-            </filestorage>
-            </zodb>
-            """ % f.name
-            self.assertRaises(ReadOnlyError, self._test, cfg)
+        path = tempfile.mktemp()
+        cfg = """
+        <zodb>
+          <filestorage>
+            path %s
+            create false
+            read-only true
+          </filestorage>
+        </zodb>
+        """ % path
+        self.assertRaises(ReadOnlyError, self._test, cfg)
 
     def test_demo_config(self):
         cfg = """
