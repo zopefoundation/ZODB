zopefoundation/ZODB

commit 6dd809d2546acc8994352ede686974b00ee879f7

Converted to a buildout and converted externals to dependencies. (Merged from 3.7 branch.)

src/ZODB/scripts/fsrefs.py
@@ -95,7 +95,19 @@ def report(oid, data, serial, missing):
         print "\toid %s %s: %r" % (oid_repr(oid), reason, description)
     print
 
-def main(path):
+def main(path=None):
+    if path is None:
+        import sys
+        import getopt
+
+        opts, args = getopt.getopt(sys.argv[1:], "v")
+        for k, v in opts:
+            if k == "-v":
+                VERBOSE += 1
+
+        path, = args
+
+    
     fs = FileStorage(path, read_only=1)
 
     # Set of oids in the index that failed to load due to POSKeyError.
@@ -142,13 +154,4 @@ def main(path):
             report(oid, data, serial, missing)
 
 if __name__ == "__main__":
-    import sys
-    import getopt
-
-    opts, args = getopt.getopt(sys.argv[1:], "v")
-    for k, v in opts:
-        if k == "-v":
-            VERBOSE += 1
-
-    path, = args
-    main(path)
+    main()

DEPENDENCIES.cfg
@@ -1,4 +1,0 @@
-ZEO
-ZODB
-ZODB-Scripts
-ZopeUndo

MANIFEST
@@ -1,379 +1,0 @@
-LICENSE.txt
-MANIFEST
-MANIFEST.in
-NEWS.txt
-README.txt
-log.ini
-setup.py
-test.py
-doc/ACKS
-doc/Makefile
-doc/README.txt
-doc/storage.pdf
-doc/storage.tex
-doc/zdctl.txt
-doc/zodb.pdf
-doc/ZConfig/Makefile
-doc/ZConfig/README.txt
-doc/ZConfig/schema.dtd
-doc/ZConfig/xmlmarkup.perl
-doc/ZConfig/xmlmarkup.sty
-doc/ZConfig/zconfig.pdf
-doc/ZConfig/zconfig.tex
-doc/ZEO/README.txt
-doc/ZEO/ZopeREADME.txt
-doc/ZEO/cache.txt
-doc/ZEO/howto.txt
-doc/ZEO/trace.txt
-doc/guide/README
-doc/guide/TODO
-doc/guide/admin.tex
-doc/guide/chatter.py
-doc/guide/gfdl.tex
-doc/guide/indexing.tex
-doc/guide/introduction.tex
-doc/guide/links.tex
-doc/guide/modules.tex
-doc/guide/prog-zodb.tex
-doc/guide/storages.tex
-doc/guide/transactions.tex
-doc/guide/zeo.tex
-doc/guide/zodb.tex
-src/BTrees/BTreeItemsTemplate.c
-src/BTrees/BTreeModuleTemplate.c
-src/BTrees/BTreeTemplate.c
-src/BTrees/BucketTemplate.c
-src/BTrees/IIBTree.py
-src/BTrees/IOBTree.py
-src/BTrees/Interfaces.py
-src/BTrees/Length.py
-src/BTrees/Maintainer.txt
-src/BTrees/MergeTemplate.c
-src/BTrees/OIBTree.py
-src/BTrees/OOBTree.py
-src/BTrees/SetOpTemplate.c
-src/BTrees/SetTemplate.c
-src/BTrees/TreeSetTemplate.c
-src/BTrees/_IIBTree.c
-src/BTrees/_IOBTree.c
-src/BTrees/_OIBTree.c
-src/BTrees/_OOBTree.c
-src/BTrees/__init__.py
-src/BTrees/_fsBTree.c
-src/BTrees/check.py
-src/BTrees/intkeymacros.h
-src/BTrees/intvaluemacros.h
-src/BTrees/objectkeymacros.h
-src/BTrees/objectvaluemacros.h
-src/BTrees/sorters.c
-src/BTrees/tests/__init__.py
-src/BTrees/tests/testBTrees.py
-src/BTrees/tests/testBTreesUnicode.py
-src/BTrees/tests/testConflict.py
-src/BTrees/tests/testSetOps.py
-src/BTrees/tests/test_btreesubclass.py
-src/BTrees/tests/test_check.py
-src/BTrees/tests/test_compare.py
-src/Persistence/_Persistence.c
-src/Persistence/__init__.py
-src/Persistence/mapping.py
-src/Persistence/tests/__init__.py
-src/Persistence/tests/testPersistent.py
-src/Persistence/tests/test_ExtensionClass.py
-src/Persistence/tests/test_mapping.py
-src/ThreadedAsync/LoopCallback.py
-src/ThreadedAsync/__init__.py
-src/ZConfig/BRANCHES.txt
-src/ZConfig/__init__.py
-src/ZConfig/cfgparser.py
-src/ZConfig/cmdline.py
-src/ZConfig/datatypes.py
-src/ZConfig/info.py
-src/ZConfig/loader.py
-src/ZConfig/matcher.py
-src/ZConfig/schema.py
-src/ZConfig/substitution.py
-src/ZConfig/url.py
-src/ZConfig/components/__init__.py
-src/ZConfig/components/basic/__init__.py
-src/ZConfig/components/basic/component.xml
-src/ZConfig/components/basic/mapping.py
-src/ZConfig/components/basic/mapping.xml
-src/ZConfig/components/basic/tests/__init__.py
-src/ZConfig/components/basic/tests/test_mapping.py
-src/ZConfig/components/logger/__init__.py
-src/ZConfig/components/logger/abstract.xml
-src/ZConfig/components/logger/base-logger.xml
-src/ZConfig/components/logger/component.xml
-src/ZConfig/components/logger/datatypes.py
-src/ZConfig/components/logger/eventlog.xml
-src/ZConfig/components/logger/factory.py
-src/ZConfig/components/logger/handlers.py
-src/ZConfig/components/logger/handlers.xml
-src/ZConfig/components/logger/logger.py
-src/ZConfig/components/logger/logger.xml
-src/ZConfig/components/logger/loghandler.py
-src/ZConfig/components/logger/tests/__init__.py
-src/ZConfig/components/logger/tests/test_logger.py
-src/ZConfig/doc/Makefile
-src/ZConfig/doc/README.txt
-src/ZConfig/doc/schema.dtd
-src/ZConfig/doc/xmlmarkup.perl
-src/ZConfig/doc/xmlmarkup.sty
-src/ZConfig/doc/zconfig.pdf
-src/ZConfig/doc/zconfig.tex
-src/ZConfig/scripts/zconfig
-src/ZConfig/tests/__init__.py
-src/ZConfig/tests/runtests.bat
-src/ZConfig/tests/runtests.py
-src/ZConfig/tests/support.py
-src/ZConfig/tests/test_cfgimports.py
-src/ZConfig/tests/test_cmdline.py
-src/ZConfig/tests/test_config.py
-src/ZConfig/tests/test_datatypes.py
-src/ZConfig/tests/test_loader.py
-src/ZConfig/tests/test_schema.py
-src/ZConfig/tests/test_subst.py
-src/ZConfig/tests/input/base-datatype1.xml
-src/ZConfig/tests/input/base-datatype2.xml
-src/ZConfig/tests/input/base-keytype1.xml
-src/ZConfig/tests/input/base-keytype2.xml
-src/ZConfig/tests/input/base.xml
-src/ZConfig/tests/input/include.conf
-src/ZConfig/tests/input/inner.conf
-src/ZConfig/tests/input/library.xml
-src/ZConfig/tests/input/logger.xml
-src/ZConfig/tests/input/outer.conf
-src/ZConfig/tests/input/simple.conf
-src/ZConfig/tests/input/simple.xml
-src/ZConfig/tests/input/simplesections.conf
-src/ZConfig/tests/input/simplesections.xml
-src/ZConfig/tests/library/README.txt
-src/ZConfig/tests/library/__init__.py
-src/ZConfig/tests/library/thing/__init__.py
-src/ZConfig/tests/library/thing/component.xml
-src/ZConfig/tests/library/thing/extras/extras.xml
-src/ZConfig/tests/library/widget/__init__.py
-src/ZConfig/tests/library/widget/component.xml
-src/ZConfig/tests/library/widget/extra.xml
-src/ZEO/ClientStorage.py
-src/ZEO/ClientStub.py
-src/ZEO/CommitLog.py
-src/ZEO/DebugServer.py
-src/ZEO/Exceptions.py
-src/ZEO/README.txt
-src/ZEO/ServerStub.py
-src/ZEO/StorageServer.py
-src/ZEO/TransactionBuffer.py
-src/ZEO/__init__.py
-src/ZEO/cache.py
-src/ZEO/component.xml
-src/ZEO/mkzeoinst.py
-src/ZEO/monitor.py
-src/ZEO/runzeo.py
-src/ZEO/schema.xml
-src/ZEO/simul.py
-src/ZEO/stats.py
-src/ZEO/util.py
-src/ZEO/version.txt
-src/ZEO/zeoctl.py
-src/ZEO/zeoctl.xml
-src/ZEO/zeopasswd.py
-src/ZEO/auth/__init__.py
-src/ZEO/auth/auth_digest.py
-src/ZEO/auth/base.py
-src/ZEO/auth/hmac.py
-src/ZEO/tests/Cache.py
-src/ZEO/tests/CommitLockTests.py
-src/ZEO/tests/ConnectionTests.py
-src/ZEO/tests/InvalidationTests.py
-src/ZEO/tests/TestThread.py
-src/ZEO/tests/ThreadTests.py
-src/ZEO/tests/__init__.py
-src/ZEO/tests/auth_plaintext.py
-src/ZEO/tests/deadlock.py
-src/ZEO/tests/forker.py
-src/ZEO/tests/multi.py
-src/ZEO/tests/speed.py
-src/ZEO/tests/stress.py
-src/ZEO/tests/testAuth.py
-src/ZEO/tests/testConnection.py
-src/ZEO/tests/testMonitor.py
-src/ZEO/tests/testTransactionBuffer.py
-src/ZEO/tests/testZEO.py
-src/ZEO/tests/testZEOOptions.py
-src/ZEO/tests/test_cache.py
-src/ZEO/tests/zeoserver.py
-src/ZEO/zrpc/__init__.py
-src/ZEO/zrpc/_hmac.py
-src/ZEO/zrpc/client.py
-src/ZEO/zrpc/connection.py
-src/ZEO/zrpc/error.py
-src/ZEO/zrpc/log.py
-src/ZEO/zrpc/marshal.py
-src/ZEO/zrpc/server.py
-src/ZEO/zrpc/smac.py
-src/ZEO/zrpc/trigger.py
-src/ZODB/ActivityMonitor.py
-src/ZODB/BaseStorage.py
-src/ZODB/ConflictResolution.py
-src/ZODB/Connection.py
-src/ZODB/DB.py
-src/ZODB/DemoStorage.py
-src/ZODB/ExportImport.py
-src/ZODB/MappingStorage.py
-src/ZODB/Mount.py
-src/ZODB/POSException.py
-src/ZODB/TmpStore.py
-src/ZODB/UndoLogCompatible.py
-src/ZODB/ZApplication.py
-src/ZODB/__init__.py
-src/ZODB/broken.py
-src/ZODB/component.xml
-src/ZODB/config.py
-src/ZODB/config.xml
-src/ZODB/conversionhack.py
-src/ZODB/dbmStorage.py
-src/ZODB/fsIndex.py
-src/ZODB/fsrecover.py
-src/ZODB/fstools.py
-src/ZODB/interfaces.py
-src/ZODB/lock_file.py
-src/ZODB/serialize.py
-src/ZODB/storage.xml
-src/ZODB/subtransactions.txt
-src/ZODB/transact.py
-src/ZODB/utils.py
-src/ZODB/winlock.c
-src/ZODB/FileStorage/FileStorage.py
-src/ZODB/FileStorage/__init__.py
-src/ZODB/FileStorage/format.py
-src/ZODB/FileStorage/fsdump.py
-src/ZODB/FileStorage/fspack.py
-src/ZODB/tests/BasicStorage.py
-src/ZODB/tests/ConflictResolution.py
-src/ZODB/tests/Corruption.py
-src/ZODB/tests/HistoryStorage.py
-src/ZODB/tests/IteratorStorage.py
-src/ZODB/tests/LocalStorage.py
-src/ZODB/tests/MTStorage.py
-src/ZODB/tests/MinPO.py
-src/ZODB/tests/PackableStorage.py
-src/ZODB/tests/PersistentStorage.py
-src/ZODB/tests/ReadOnlyStorage.py
-src/ZODB/tests/RecoveryStorage.py
-src/ZODB/tests/RevisionStorage.py
-src/ZODB/tests/StorageTestBase.py
-src/ZODB/tests/Synchronization.py
-src/ZODB/tests/TransactionalUndoStorage.py
-src/ZODB/tests/TransactionalUndoVersionStorage.py
-src/ZODB/tests/VersionStorage.py
-src/ZODB/tests/__init__.py
-src/ZODB/tests/dangle.py
-src/ZODB/tests/sampledm.py
-src/ZODB/tests/speed.py
-src/ZODB/tests/testActivityMonitor.py
-src/ZODB/tests/testBroken.py
-src/ZODB/tests/testCache.py
-src/ZODB/tests/testConfig.py
-src/ZODB/tests/testConnection.py
-src/ZODB/tests/testDB.py
-src/ZODB/tests/testDemoStorage.py
-src/ZODB/tests/testFileStorage.py
-src/ZODB/tests/testMappingStorage.py
-src/ZODB/tests/testPersistentList.py
-src/ZODB/tests/testPersistentMapping.py
-src/ZODB/tests/testRecover.py
-src/ZODB/tests/testSerialize.py
-src/ZODB/tests/testSubTransaction.py
-src/ZODB/tests/testTimeStamp.py
-src/ZODB/tests/testUtils.py
-src/ZODB/tests/testZODB.py
-src/ZODB/tests/test_cache.py
-src/ZODB/tests/test_datamanageradapter.py
-src/ZODB/tests/test_storage.py
-src/ZODB/tests/testfsIndex.py
-src/ZODB/tests/testmvcc.py
-src/ZODB/tests/util.py
-src/ZODB/tests/warnhook.py
-src/ZopeUndo/Prefix.py
-src/ZopeUndo/__init__.py
-src/ZopeUndo/tests/__init__.py
-src/ZopeUndo/tests/testPrefix.py
-src/persistent/README.txt
-src/persistent/TimeStamp.c
-src/persistent/__init__.py
-src/persistent/cPersistence.c
-src/persistent/cPersistence.h
-src/persistent/cPickleCache.c
-src/persistent/dict.py
-src/persistent/interfaces.py
-src/persistent/list.py
-src/persistent/mapping.py
-src/persistent/ring.c
-src/persistent/ring.h
-src/persistent/wref.py
-src/persistent/tests/__init__.py
-src/persistent/tests/persistent.txt
-src/persistent/tests/persistenttestbase.py
-src/persistent/tests/testPersistent.py
-src/persistent/tests/test_PickleCache.py
-src/persistent/tests/test_list.py
-src/persistent/tests/test_overriding_attrs.py
-src/persistent/tests/test_persistent.py
-src/persistent/tests/test_pickle.py
-src/persistent/tests/test_wref.py
-src/scripts/README.txt
-src/scripts/analyze.py
-src/scripts/checkbtrees.py
-src/scripts/fsdump.py
-src/scripts/fsrefs.py
-src/scripts/fsstats.py
-src/scripts/fstail.py
-src/scripts/fstest.py
-src/scripts/migrate.py
-src/scripts/netspace.py
-src/scripts/parsezeolog.py
-src/scripts/repozo.py
-src/scripts/space.py
-src/scripts/timeout.py
-src/scripts/zeopack.py
-src/scripts/zeoqueue.py
-src/scripts/zeoreplay.py
-src/scripts/zeoserverlog.py
-src/scripts/zeoup.py
-src/scripts/zodbload.py
-src/scripts/tests/test-checker.fs
-src/scripts/tests/testfstest.py
-src/scripts/tests/testzeopack.py
-src/transaction/README.txt
-src/transaction/__init__.py
-src/transaction/_manager.py
-src/transaction/_transaction.py
-src/transaction/interfaces.py
-src/transaction/notes.txt
-src/transaction/util.py
-src/transaction/tests/__init__.py
-src/transaction/tests/abstestIDataManager.py
-src/transaction/tests/test_SampleDataManager.py
-src/transaction/tests/test_register_compat.py
-src/transaction/tests/test_transaction.py
-src/transaction/tests/test_util.py
-src/zLOG/EventLogger.py
-src/zLOG/__init__.py
-src/zLOG/tests/__init__.py
-src/zLOG/tests/test_logging.py
-src/zLOG/tests/testzLog.py
-src/zdaemon/__init__.py
-src/zdaemon/component.xml
-src/zdaemon/sample.conf
-src/zdaemon/schema.xml
-src/zdaemon/zdctl.py
-src/zdaemon/zdoptions.py
-src/zdaemon/zdrun.py
-src/zdaemon/tests/__init__.py
-src/zdaemon/tests/donothing.sh
-src/zdaemon/tests/nokill.py
-src/zdaemon/tests/parent.py
-src/zdaemon/tests/testzdoptions.py
-src/zdaemon/tests/testzdrun.py

MANIFEST.in
@@ -1,9 +1,0 @@
-include MANIFEST MANIFEST.in
-include *.txt
-include test.py log.ini
-recursive-include src *.h *.c *.xml *.txt *.sh *.conf *.bat
-include src/ZConfig/scripts/zconfig
-graft doc
-graft src/scripts
-graft src/ZConfig/doc
-global-exclude .cvsignore

PACKAGE.cfg
@@ -1,17 +1,0 @@
-<distribution>
-  doc
-  log.ini
-  test.py
-  COPYRIGHT.txt
-  LICENSE.txt
-  NEWS.txt
-  README.txt
-</distribution>
-
-<collection>
-  doc           -
-  setup.py      -
-  src           -
-  zpkg.conf     -
-  buildsupport  -
-</collection>

PUBLICATION.cfg
@@ -1,23 +1,0 @@
-Metadata-version: 1.1
-Name: ZODB3
-License: ZPL 2.1
-Home-page: http://www.zope.org/Wikis/ZODB
-Summary: Zope Object Database: object database and persistence
-Description:
-        The Zope Object Database provides an object-oriented database
-        for Python that provides a high-degree of transparency.
-        Applications can take advantage of object database features
-        with few, if any, changes to application logic.  ZODB includes
-        features such as a plugable storage interface, rich
-        transaction support, and undo.
-Maintainer: ZODB Developers
-Maintainer-email: zodb-dev@zope.org
-Platform: any
-Classifier: Development Status :: 5 - Production/Stable
-Classifier: Intended Audience :: Developers
-Classifier: License :: OSI Approved :: Zope Public License
-Classifier: Programming Language :: Python
-Classifier: Topic :: Database
-Classifier: Topic :: Software Development :: Libraries :: Python Modules
-Classifier: Operating System :: Microsoft :: Windows
-Classifier: Operating System :: Unix

bootstrap.py
@@ -1,0 +1,52 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Bootstrap a buildout-based project
+
+Simply run this script in a directory containing a buildout.cfg.
+The script accepts buildout command-line options, so you can
+use the -c option to specify an alternate configuration file.
+
+$Id$
+"""
+
+import os, shutil, sys, tempfile, urllib2
+
+tmpeggs = tempfile.mkdtemp()
+
+ez = {}
+exec urllib2.urlopen('http://peak.telecommunity.com/dist/ez_setup.py'
+                     ).read() in ez
+ez['use_setuptools'](to_dir=tmpeggs, download_delay=0)
+
+import pkg_resources
+
+cmd = 'from setuptools.command.easy_install import main; main()'
+if sys.platform == 'win32':
+    cmd = '"%s"' % cmd # work around spawn lamosity on windows
+
+ws = pkg_resources.working_set
+assert os.spawnle(
+    os.P_WAIT, sys.executable, sys.executable,
+    '-c', cmd, '-mqNxd', tmpeggs, 'zc.buildout',
+    dict(os.environ,
+         PYTHONPATH=
+         ws.find(pkg_resources.Requirement.parse('setuptools')).location
+         ),
+    ) == 0
+
+ws.add_entry(tmpeggs)
+ws.require('zc.buildout')
+import zc.buildout.buildout
+zc.buildout.buildout.main(sys.argv[1:] + ['bootstrap'])
+shutil.rmtree(tmpeggs)

buildout.cfg
@@ -1,0 +1,13 @@
+[buildout]
+develop = .
+parts = test scripts
+find-links = http://download.zope.org/distribution/
+
+[test]
+recipe = zc.recipe.testrunner
+eggs = ZODB3
+
+[scripts]
+recipe = zc.recipe.egg
+eggs = ZODB3
+interpreter = py

commit 29f75cc6130052290c232ae6db7177cc0dcc1dde

scripts/fsrefs: Fix -v
For this script verbose mode was added in 57bca1f0 (Add -v option to
control whether tracebacks are printed.) and was later broken in
6dd809d2 (Converted to a buildout and converted externals to
dependencies.) - the change forgot to add global specifier for VERBOSE
variable while moving its assignment into function context.

As a result currently we have

    $ python ./src/ZODB/scripts/fsrefs.py -v data.fs
    Traceback (most recent call last):
      File "./src/ZODB/scripts/fsrefs.py", line 155, in <module>
        main()
      File "./src/ZODB/scripts/fsrefs.py", line 104, in main
        VERBOSE += 1
    UnboundLocalError: local variable 'VERBOSE' referenced before assignment

Fix it.

src/ZODB/scripts/fsrefs.py
@@ -94,6 +94,7 @@ def report(oid, data, serial, missing):
     print()
 
 def main(path=None):
+    global VERBOSE
     if path is None:
         import sys
         import getopt