79f7f4ac6d1bf2f1f7930727495459b8847c1c31
Jim Fulton <jim@zope.com>
2014-07-13 17:02:00 -0400

    4.0.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ to application logic.  ZODB includes features such as a plugable storage
 interface, rich transaction support, and undo.
 """
 
-VERSION = "4.0.1dev"
+VERSION = "4.0.1"
 
 import os
 import sys
