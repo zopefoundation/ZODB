e40c85352274826aae83e095c24444b1ab50f1cf
Jim Fulton <jim@jimfulton.info>
2017-04-08 08:42:27 -0400

    Back to development: 5.2.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-version = '5.2.1'
+version = '5.2.2.dev0'
 
 import os
 from setuptools import setup, find_packages
