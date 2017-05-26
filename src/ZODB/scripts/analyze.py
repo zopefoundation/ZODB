af2af85c331a9a306ce53dc3ac4c6f2c1b6efefa
Douglas Anderson <djanderson@users.noreply.github.com>
2017-05-25 23:14:25 -0600

    In py3, dict_keys type has no attribute sort
    Using `ZODB.scripts.analyze` under python3, I ran into the follow issue:
    
    ```  File "/usr/local/lib/python3.5/dist-packages/ZODB/scripts/analyze.py", line 71, in report
        typemap.sort()
    AttributeError: 'dict_keys' object has no attribute 'sort'```
    
    This commit uses the builtin `sorted` function instead.

--- src/ZODB/scripts/analyze.py
+++ src/ZODB/scripts/analyze.py
@@ -67,8 +67,7 @@ def report(rep):
     fmts = "%46s %7d %8dk %5.1f%% %7.2f" # summary format
     print(fmt % ("Class Name", "Count", "TBytes", "Pct", "AvgSize"))
     print(fmt % ('-'*46, '-'*7, '-'*9, '-'*5, '-'*7))
-    typemap = rep.TYPEMAP.keys()
-    typemap.sort()
+    typemap = sorted(rep.TYPEMAP.keys())
     cumpct = 0.0
     for t in typemap:
         pct = rep.TYPESIZE[t] * 100.0 / rep.DBYTES
