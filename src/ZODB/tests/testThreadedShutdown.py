dc65b97e2cbecead3f7f7c1046a852141bc9c008
Jim Fulton <jim@jimfulton.info>
2018-10-14 15:55:11 +0000

    Use an event to wait for the thread to start before shutting down.
    Also, use a daemonic thread so that the test runner doesn't hand
    waiting for the thread to die.

--- src/ZODB/tests/testThreadedShutdown.py
+++ src/ZODB/tests/testThreadedShutdown.py
@@ -13,14 +13,15 @@ class ZODBClientThread(threading.Thread):
     def __init__(self, db, test):
         threading.Thread.__init__(self)
         self._exc_info = None
-        # self.setDaemon(1)
+        self.setDaemon(True)
         self.db = db
         self.test = test
+        self.event = threading.Event()
 
     def run(self):
         conn = self.db.open()
         conn.sync()
-
+        self.event.set()
         time.sleep(15)
 
         # conn.close calls self.transaction_manager.unregisterSynch(self)
@@ -42,6 +43,7 @@ class ShutdownTest(ZODB.tests.util.TestCase):
     def check_shutdown(self):
         client_thread = ZODBClientThread(self._db, self)
         client_thread.start()
+        client_thread.event.wait()
         # calls conn._release_resources, that calls conn.close(),
         # that calls conn.transaction_manager.unregisterSynch(self),
         # but from a different thread, so transaction_manager._synchs
