8e4591ab0b26ddd27fdd979d976afacb591f1f8e
Michael Howitz <icemac@gmx.net>
2023-08-18 08:34:27 +0200

    Update src/ZODB/blob.py

--- src/ZODB/blob.py
+++ src/ZODB/blob.py
@@ -942,7 +942,6 @@ if sys.platform == 'win32':
                 filename = os.path.join(dirpath, filename)
                 remove_committed(filename)
         shutil.rmtree(path)
-        
     link_or_copy = shutil.copy
 else:
     remove_committed = os.remove
